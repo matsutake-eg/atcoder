19e1d0ae346cc6510b3ab17789fdad263cc9088e
matsutake <matsutake.pg@gmail.com>
2019-04-13 18:59:40 +0900

    メモリ使用量を減らすように修正

--- abs/arc089_a/main.go
+++ abs/arc089_a/main.go
@@ -2,17 +2,6 @@ package main
 
 import "fmt"
 
-func scanMultiNums(multi_nums_len int) (multi_nums [][]int) {
-	multi_nums = append(multi_nums, []int{0, 0, 0})
-
-	var t, x, y int
-	for i := 0; i < multi_nums_len; i++ {
-		fmt.Scan(&t, &x, &y)
-		multi_nums = append(multi_nums, []int{t, x, y})
-	}
-	return
-}
-
 func abs(x int) int {
 	if x < 0 {
 		return -x
@@ -23,24 +12,30 @@ func abs(x int) int {
 func main() {
 	var n int
 	fmt.Scan(&n)
-	multi_nums := scanMultiNums(n)
 
 	var is_travel bool
-	var dt, dx, dy int
+	var t, x, y, t_b, x_b, y_b, dt, dx, dy int
 
-	for i := 1; i < n+1; i++ {
-		dt = multi_nums[i][0] - multi_nums[i-1][0]
-		dx = abs(multi_nums[i][1] - multi_nums[i-1][1])
-		dy = abs(multi_nums[i][2] - multi_nums[i-1][2])
+	for i := 0; i < n; i++ {
+		fmt.Scan(&t, &x, &y)
+		dt = t - t_b
+		dx = abs(x - x_b)
+		dy = abs(y - y_b)
 
 		if dt < dx || dt < dy {
 			is_travel = false
-			break
 		} else if dt%2 != (dx+dy)%2 {
 			is_travel = false
-			break
 		} else {
 			is_travel = true
+		}
+
+		if is_travel {
+			t_b = t
+			x_b = x
+			y_b = y
+		} else {
+			break
 		}
 	}
 
