c1d3637f9ea0e605818d8b77d5684764b2f1ac55
matsutake <matsutake.pg@gmail.com>
2019-04-15 19:55:54 +0900

    グリッドに関する処理の問題を完了

---
+++ recommend/abc096_c/main.go
@@ -0,0 +1,77 @@
+package main
+
+import "fmt"
+
+func scanStrs(h int) (strs [][]rune) {
+	strs = make([][]rune, h)
+	var s string
+	for i := range strs {
+		fmt.Scan(&s)
+		strs[i] = []rune(s)
+	}
+	return
+}
+
+func drawable(strs [][]rune, i, j int) bool {
+	if strs[i][j] == '.' {
+		return true
+	}
+
+	result := false
+	if i-1 >= 0 {
+		if strs[i-1][j] == '#' {
+			result = true
+		}
+	}
+
+	if j-1 >= 0 {
+		if strs[i][j-1] == '#' {
+			result = true
+		}
+	}
+
+	if j+1 < len(strs[0]) {
+		if strs[i][j+1] == '#' {
+			result = true
+		}
+	}
+
+	if i+1 < len(strs) {
+		if strs[i+1][j] == '#' {
+			result = true
+		}
+	}
+
+	return result
+}
+
+func main() {
+	var h, w int
+	fmt.Scan(&h, &w)
+	strs := scanStrs(h)
+
+	is_complete := true
+	for i := 0; i < h; i++ {
+		if !is_complete {
+			break
+		}
+
+		for j := 0; j < w; j++ {
+			if !is_complete {
+				break
+			}
+
+			if drawable(strs, i, j) {
+				is_complete = true
+			} else {
+				is_complete = false
+			}
+		}
+	}
+
+	if is_complete {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+}
