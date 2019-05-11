8530632bcc2bf951655a2e63db2bc268ebea8b5d
matsutake <matsutake.pg@gmail.com>
2019-05-11 10:55:39 +0900

    過去問

--- abc113/c/main.go
+++ abc113/c/main.go
@@ -9,37 +9,26 @@ import (
 )
 
 type City struct {
-	Index        int
-	PrefectureNo int
-	Year         int
-	ServiceNo    string
+	Index      int
+	Prefecture int
+	Year       int
 }
 
-type By func(c1, c2 *City) bool
+type Cities []City
 
-func (by By) Sort(cities []City) {
-	cs := &citySorter{
-		cities: cities,
-		by:     by,
+func (cs Cities) Len() int {
+	return len(cs)
+}
+
+func (cs Cities) Swap(i, j int) {
+	cs[i], cs[j] = cs[j], cs[i]
+}
+
+func (cs Cities) Less(i, j int) bool {
+	if cs[i].Prefecture == cs[j].Prefecture {
+		return cs[i].Year < cs[j].Year
 	}
-	sort.Sort(cs)
-}
-
-type citySorter struct {
-	cities []City
-	by     func(c1, c2 *City) bool
-}
-
-func (c *citySorter) Len() int {
-	return len(c.cities)
-}
-
-func (c *citySorter) Swap(i, j int) {
-	c.cities[i], c.cities[j] = c.cities[j], c.cities[i]
-}
-
-func (c *citySorter) Less(i, j int) bool {
-	return c.by(&c.cities[i], &c.cities[j])
+	return cs[i].Prefecture < cs[j].Prefecture
 }
 
 func main() {
@@ -48,36 +37,29 @@ func main() {
 
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Split(bufio.ScanWords)
-	cs := make([]City, m)
+	var cs Cities = make([]City, m)
 	for i := range cs {
 		cs[i].Index = i
 		sc.Scan()
-		cs[i].PrefectureNo, _ = strconv.Atoi(sc.Text())
+		cs[i].Prefecture, _ = strconv.Atoi(sc.Text())
 		sc.Scan()
 		cs[i].Year, _ = strconv.Atoi(sc.Text())
 	}
 
-	By(func(c1, c2 *City) bool {
-		if c1.PrefectureNo == c2.PrefectureNo {
-			return c1.Year < c2.Year
+	sort.Sort(cs)
+
+	xs := make([]string, m)
+	p, c := 0, 0
+	for _, v := range cs {
+		if p != v.Prefecture {
+			p = v.Prefecture
+			c = 0
 		}
-		return c1.PrefectureNo < c2.PrefectureNo
-	}).Sort(cs)
-	p := 0
-	var y int
-	for i, v := range cs {
-		if p != v.PrefectureNo {
-			p = v.PrefectureNo
-			y = 0
-		}
-		y++
-		cs[i].ServiceNo = fmt.Sprintf("%06d%06d", p, y)
+		c++
+		xs[v.Index] = fmt.Sprintf("%06d%06d", p, c)
 	}
 
-	By(func(c1, c2 *City) bool {
-		return c1.Index < c2.Index
-	}).Sort(cs)
-	for _, v := range cs {
-		fmt.Println(v.ServiceNo)
+	for _, v := range xs {
+		fmt.Println(v)
 	}
 }
