b32a2fdfc9e46645ad5a3fc0e496a42ec2ff06f1
松田 健志 <takeshi_matsuda@MAC0047-2190452.local>
2019-05-31 08:56:43 +0900

    過去問

---
+++ abc125/d/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Split(bufio.ScanWords)
+	var (
+		sum       int64
+		min       = 10000000001
+		isNegaOdd bool
+	)
+	for i := 0; i < n; i++ {
+		sc.Scan()
+		a, _ := strconv.Atoi(sc.Text())
+		if a < 0 {
+			isNegaOdd = !isNegaOdd
+			a = -a
+		}
+		sum += int64(a)
+		if a < min {
+			min = a
+		}
+	}
+	if isNegaOdd {
+		sum -= int64(min * 2)
+	}
+	fmt.Println(sum)
+}
