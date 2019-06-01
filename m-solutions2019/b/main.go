f150d19d6fff456b308dcd4b1e4dff468de69273
matsutake <matsutake.pg@gmail.com>
2019-06-01 23:43:09 +0900

    リファクタ

--- m-solutions2019/b/main.go
+++ m-solutions2019/b/main.go
@@ -8,8 +8,8 @@ import (
 func main() {
 	var s string
 	fmt.Scan(&s)
-	wins := strings.Count(s, "o")
-	if 15-len(s)+wins >= 8 {
+	loses := strings.Count(s, "x")
+	if 15-loses >= 8 {
 		fmt.Println("YES")
 	} else {
 		fmt.Println("NO")
