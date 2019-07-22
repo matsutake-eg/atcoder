e80d887e347e8de97be618bb30b44bea61e70d2a
matsutake <matsutake.pg@gmail.com>
2019-07-23 07:34:04 +0900

    過去問

--- abc099/c/main.go
+++ abc099/c/main.go
@@ -6,14 +6,18 @@ func main() {
 	var n int
 	fmt.Scan(&n)
 
-	ans, x := 0, 1
-	for x <= n {
-		x *= 9
+	ans := n
+	for i := 0; i <= n; i++ {
+		sum := 0
+		for x := i; x > 0; x /= 6 {
+			sum += x % 6
+		}
+		for x := n - i; x > 0; x /= 9 {
+			sum += x % 9
+		}
+		if sum < ans {
+			ans = sum
+		}
 	}
-	n -= x
-	ans++
-	if n == 0 {
-		fmt.Println(ans)
-		return
-	}
+	fmt.Println(ans)
 }
