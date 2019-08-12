a29cfbad0aa591f4695c2ebe193d89ca844d4c8c
matsutake <matsutake.pg@gmail.com>
2019-08-12 22:46:23 +0900

    優先度付きキュー追加

---
+++ snippets/priorityQueue.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+var pq []int
+
+func push(x int) {
+	p := sort.Search(len(pq), func(i int) bool { return pq[i] <= x })
+	if p == len(pq) {
+		pq = append(pq, x)
+		return
+	}
+	pq = append(pq[:p+1], pq[p:]...)
+	pq[p] = x
+}
+
+func pop() int {
+	ans := pq[0]
+	pq = pq[1:]
+	return ans
+}
+
+func main() {
+	pq = make([]int, 0, 5)
+	push(1)
+	fmt.Println(pq)
+	push(3)
+	fmt.Println(pq)
+	push(2)
+	fmt.Println(pq)
+	push(1)
+	fmt.Println(pq)
+	fmt.Println(pop())
+	fmt.Println(pq)
+	fmt.Println(pop())
+	fmt.Println(pq)
+}
