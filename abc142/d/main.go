712f6fdcd9173f51322cedaadbdff1879650783a
matsutake <matsutake.pg@gmail.com>
2019-09-29 00:11:59 +0900

    リファクタ

--- abc142/d/main.go
+++ abc142/d/main.go
@@ -2,27 +2,26 @@ package main
 
 import "fmt"
 
-func gcd(x, y int) int {
-	for y != 0 {
-		x, y = y, x%y
-	}
-	return x
-}
-
 func main() {
 	var a, b int
 	fmt.Scan(&a, &b)
 
-	g := gcd(a, b)
-	ans := make(map[int]bool)
-	for i := 2; i*i <= g; i++ {
-		for g%i == 0 {
-			g /= i
-			ans[i] = true
+	m := make(map[int]bool)
+	for i := 2; i*i <= a; i++ {
+		for a%i == 0 {
+			a /= i
+			m[i] = true
 		}
 	}
-	if g > 1 {
-		ans[g] = true
+	if a > 1 {
+		m[a] = true
 	}
-	fmt.Println(len(ans) + 1)
+
+	ans := 1
+	for k := range m {
+		if b%k == 0 {
+			ans++
+		}
+	}
+	fmt.Println(ans)
 }
