0fb9ace2ed9461db6d8c35471b2f8fc8167b1adb
matsutake <matsutake.pg@gmail.com>
2019-11-23 23:40:54 +0900

    高速化

--- abc132/d/main.go
+++ abc132/d/main.go
@@ -1,42 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 const mod = 1000000007
 
-var (
-	n, k int
-	c    [][]int
-)
+var fac, finv, inv []int
+var wr = bufio.NewWriter(os.Stdout)
 
-func initCmb() {
-	max := n - k + 1
-	if v := k - 1; v > max {
-		max = v
-	}
-
-	c = make([][]int, max+1)
-	for i := range c {
-		c[i] = make([]int, i+1)
-		c[i][0], c[i][len(c[i])-1] = 1, 1
-		for j := 1; j < len(c[i])-1; j++ {
-			c[i][j] = (c[i-1][j-1] + c[i-1][j]) % mod
-		}
+func initCmb(x int) {
+	fac, finv, inv = make([]int, x+1), make([]int, x+1), make([]int, x+1)
+	fac[0], fac[1] = 1, 1
+	finv[0], finv[1] = 1, 1
+	inv[1] = 1
+	for i := 2; i <= x; i++ {
+		fac[i] = fac[i-1] * i % mod
+		inv[i] = mod - inv[mod%i]*(mod/i)%mod
+		finv[i] = finv[i-1] * inv[i] % mod
 	}
 }
 
-func cmb(a, b int) int {
-	if b > a {
+func cmb(x, y int) int {
+	if y > x {
 		return 0
 	}
-	return c[a][b]
+	return fac[x] * (finv[y] * finv[x-y] % mod) % mod
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
 
 func main() {
+	var n, k int
 	fmt.Scan(&n, &k)
 
-	initCmb()
+	initCmb(max(n-k+1, k-1))
 	for i := 1; i <= k; i++ {
-		fmt.Println(cmb(n-k+1, i) * cmb(k-1, i-1) % mod)
+		wr.WriteString(strconv.Itoa(cmb(n-k+1, i)*cmb(k-1, i-1)%mod) + "\n")
 	}
+	wr.Flush()
 }
