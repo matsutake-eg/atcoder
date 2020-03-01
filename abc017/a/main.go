c619c69d8536a9dc24572c60f94a43395bc9319e
matsutake <matsutake.pg@gmail.com>
2020-03-01 20:41:17 +0900

    再帰はTLE

--- abc017/a/main.go
+++ abc017/a/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	s1, e1 := scanInt(), scanInt()
+	s2, e2 := scanInt(), scanInt()
+	s3, e3 := scanInt(), scanInt()
+
+	fmt.Println(s1*e1/10 + s2*e2/10 + s3*e3/10)
 }
--- abc017/b/main.go
+++ abc017/b/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var x string
+	fmt.Scan(&x)
+
+	if x == "" {
+		fmt.Println("YES")
+		return
+	}
+
+	for i := 0; i < len(x); i++ {
+		switch x[i] {
+		case 'o':
+			continue
+		case 'k':
+			continue
+		case 'u':
+			continue
+		case 'c':
+			if i+1 < len(x) && x[i+1] == 'h' {
+				i++
+				continue
+			}
+		}
+		fmt.Println("NO")
+		return
+	}
+	fmt.Println("YES")
 }
