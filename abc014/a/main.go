7a8b82b0041a9c383b52774d64b8a2fcfd35328f
matsutake <matsutake.pg@gmail.com>
2020-03-01 23:42:49 +0900

    ビットシフト

--- abc014/a/main.go
+++ abc014/a/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+
+	i := 0
+	for {
+		if (a+i)%b == 0 {
+			fmt.Println(i)
+			return
+		}
+		i++
+	}
 }
