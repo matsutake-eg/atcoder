1cac8c5a68fae4fbca1db628a6e9f8e1e0078a82
matsutake <matsutake.pg@gmail.com>
2020-03-02 19:03:30 +0900

    過去問

--- abc010/c/main.go
+++ abc010/c/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	txa, tya := scanInt(), scanInt()
+	txb, tyb := scanInt(), scanInt()
+	t, v := scanInt(), scanInt()
+	n := scanInt()
+	for i := 0; i < n; i++ {
+		x, y := scanInt(), scanInt()
+		dxa, dya := x-txa, y-tya
+		dxb, dyb := x-txb, y-tyb
+		if math.Sqrt(float64(dxa*dxa+dya*dya))+math.Sqrt(float64(dxb*dxb+dyb*dyb)) <= float64(t*v) {
+			fmt.Println("YES")
+			return
+		}
+	}
+	fmt.Println("NO")
 }
