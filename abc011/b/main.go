441bc72b187744dbed5616bdbafdc62f33934115
matsutake <matsutake.pg@gmail.com>
2020-03-09 01:15:03 +0900

    DP引き算

---
+++ abc011/b/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var s string
+	fmt.Scan(&s)
+
+	fmt.Println(strings.ToUpper(s[:1]) + strings.ToLower(s[1:]))
+}
---
+++ abc011/c/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	var n, ng1, ng2, ng3 int
+	fmt.Scan(&n, &ng1, &ng2, &ng3)
+
+	if n == ng1 || n == ng2 || n == ng3 {
+		fmt.Println("NO")
+		return
+	}
+
+	ans := make([]int, n+1)
+	for i := range ans[:n] {
+		ans[i] = -1
+	}
+
+	nums := []int{1, 2, 3}
+	for i := n; i >= 0; i-- {
+		if i == ng1 || i == ng2 || i == ng3 || ans[i] == -1 {
+			continue
+		}
+
+		for _, num := range nums {
+			if v := i - num; v >= 0 && v != ng1 && v != ng2 && v != ng3 {
+				if ans[v] == -1 {
+					ans[v] = ans[i] + 1
+				} else {
+					ans[v] = min(ans[v], ans[i]+1)
+				}
+			}
+		}
+	}
+
+	if ans[0] != -1 && ans[0] <= 100 {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+}
