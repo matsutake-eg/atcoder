3d6c5d77d944d8aad5218dd2b99832edd8f2347d
matsutake <matsutake.pg@gmail.com>
2020-03-28 18:58:59 +0900

    全探索で画像マッチング

--- abc054/a/main.go
+++ abc054/a/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+
+	if a == 1 {
+		a += 13
+	}
+	if b == 1 {
+		b += 13
+	}
+
+	if a > b {
+		fmt.Println("Alice")
+	} else if a < b {
+		fmt.Println("Bob")
+	} else {
+		fmt.Println("Draw")
+	}
 }
--- abc054/b/main.go
+++ abc054/b/main.go
@@ -1,5 +1,55 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	iv, _ := strconv.Atoi(scanString())
+	return iv
+}
+
+func scanString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 100001), 100001*100)
+}
+
+func main() {
+	n, m := scanInt(), scanInt()
+	a := make([]string, n)
+	for i := range a {
+		a[i] = scanString()
+	}
+	b := make([]string, m)
+	for i := range b {
+		b[i] = scanString()
+	}
+
+	for ai := 0; ai < n-m+1; ai++ {
+		for aj := 0; aj < n-m+1; aj++ {
+		L:
+			for bi := 0; bi < m; bi++ {
+				for bj := 0; bj < m; bj++ {
+					if a[ai+bi][aj+bj] != b[bi][bj] {
+						break L
+					}
+					if bi == m-1 && bj == m-1 {
+						fmt.Println("Yes")
+						return
+					}
+				}
+			}
+		}
+	}
+	fmt.Println("No")
 }
