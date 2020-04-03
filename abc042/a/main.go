3556cd8ce7527db8314f296164187853311ba5c7
matsutake <matsutake.pg@gmail.com>
2020-04-04 03:47:20 +0900

    再帰で全探索

--- abc042/a/main.go
+++ abc042/a/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a, b, c int
+	fmt.Scan(&a, &b, &c)
+
+	if (a == 7 && b == 5 && c == 5) || (a == 5 && b == 7 && c == 5) || (a == 5 && b == 5 && c == 7) {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
 }
