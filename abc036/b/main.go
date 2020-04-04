45669b1ca192c7515f56863e355dbea24257eb7f
matsutake <matsutake.pg@gmail.com>
2020-04-04 13:48:18 +0900

    インデックス付きソート

--- abc036/b/main.go
+++ abc036/b/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	iv, _ := strconv.Atoi(scanString())
+	return iv
+}
+
+func scanString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 100001), 100001*100)
+}
+
+func main() {
+	n := scanInt()
+	s := make([]string, n)
+	for i := range s {
+		s[i] = scanString()
+	}
+
+	var wr = bufio.NewWriter(os.Stdout)
+	for j := 0; j < n; j++ {
+		for i := n - 1; i >= 0; i-- {
+			wr.WriteString(string(s[i][j]))
+		}
+		wr.WriteString("\n")
+	}
+	wr.Flush()
 }
--- abc036/c/main.go
+++ abc036/c/main.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
+	"sort"
+	"strconv"
+)
 
+type sit struct{ x, i int }
+type sits []sit
+
+func (s sits) Len() int           { return len(s) }
+func (s sits) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s sits) Less(i, j int) bool { return s[i].x < s[j].x }
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n := scanInt()
+	ss := sits(make([]sit, n))
+	for i := range ss {
+		ss[i].x, ss[i].i = scanInt(), i
+	}
+
+	sort.Sort(ss)
+	cnt := 0
+	ans := make([]int, n)
+	ans[ss[0].i] = 0
+	for i := 1; i < n; i++ {
+		if ss[i].x > ss[i-1].x {
+			cnt++
+		}
+		ans[ss[i].i] = cnt
+	}
+
+	var wr = bufio.NewWriter(os.Stdout)
+	for _, v := range ans {
+		wr.WriteString(strconv.Itoa(v) + "\n")
+	}
+	wr.Flush()
 }
