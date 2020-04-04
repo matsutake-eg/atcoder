853b676626ff8ce8adcdc7b3e12f9fc4e313aac1
matsutake <matsutake.pg@gmail.com>
2020-04-04 15:17:52 +0900

    並び

--- abc050/a/main.go
+++ abc050/a/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var (
+		a, b int
+		op   string
+	)
+	fmt.Scan(&a, &op, &b)
+
+	if op == "+" {
+		fmt.Println(a + b)
+	} else {
+		fmt.Println(a - b)
+	}
 }
--- abc050/b/main.go
+++ abc050/b/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n := scanInt()
+	t := make([]int, n)
+	sum := 0
+	for i := range t {
+		t[i] = scanInt()
+		sum += t[i]
+	}
+	m := scanInt()
+	var wr = bufio.NewWriter(os.Stdout)
+	for i := 0; i < m; i++ {
+		p, x := scanInt(), scanInt()
+		wr.WriteString(strconv.Itoa(sum-t[p-1]+x) + "\n")
+	}
+	wr.Flush()
 }
