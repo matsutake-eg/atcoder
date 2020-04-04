2ce7907da44ab3c9886433d45662232235d7b075
matsutake <matsutake.pg@gmail.com>
2020-04-04 18:45:40 +0900

    素因数カウント

--- abc052/b/main.go
+++ abc052/b/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	var (
+		n int
+		s string
+	)
+	fmt.Scan(&n, &s)
+
+	ans := 0
+	x := 0
+	for _, r := range s {
+		if r == 'I' {
+			x++
+		} else {
+			x--
+		}
+		ans = max(ans, x)
+	}
+	fmt.Println(ans)
 }
--- abc052/c/main.go
+++ abc052/c/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	xm := make(map[int]int)
+	for i := 2; i <= n; i++ {
+		x := i
+		for j := 2; j <= i; j++ {
+			for x%j == 0 {
+				xm[j]++
+				x /= j
+			}
+		}
+	}
+
+	const mod = 1000000007
+	ans := 1
+	for _, v := range xm {
+		ans *= v + 1
+		ans %= mod
+	}
+	fmt.Println(ans)
 }
