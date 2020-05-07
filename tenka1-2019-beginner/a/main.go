060e36d377a4abcd9d1120ca2088dcc49e92bcfc
matsutake <matsutake.pg@gmail.com>
2020-05-08 06:46:38 +0900

    合計値の全探索

--- tenka1-2019-beginner/a/main.go
+++ tenka1-2019-beginner/a/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a, b, c int
+	fmt.Scan(&a, &b, &c)
+
+	if (c > a && c < b) || (c > b && c < a) {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
 }
--- tenka1-2019-beginner/b/main.go
+++ tenka1-2019-beginner/b/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	var (
+		n, k int
+		s    string
+	)
+	fmt.Scan(&n, &s, &k)
+
+	var wr = bufio.NewWriter(os.Stdout)
+
+	for i := 0; i < n; i++ {
+		if s[i] != s[k-1] {
+			wr.WriteString("*")
+		} else {
+			wr.WriteString(string(s[i]))
+		}
+	}
+	wr.WriteString("\n")
+	wr.Flush()
 }
