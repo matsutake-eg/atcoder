15bd30ef0879eb11a71a546cec21b2dcd5b15377
matsutake <matsutake.pg@gmail.com>
2020-05-12 08:56:57 +0900

    累積和と全探索

--- ddcc2020-qual/a/main.go
+++ ddcc2020-qual/a/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var x, y int
+	fmt.Scan(&x, &y)
+
+	ans := 0
+	switch x {
+	case 1:
+		ans += 300000
+	case 2:
+		ans += 200000
+	case 3:
+		ans += 100000
+	}
+	switch y {
+	case 1:
+		ans += 300000
+	case 2:
+		ans += 200000
+	case 3:
+		ans += 100000
+	}
+	if x == 1 && y == 1 {
+		ans += 400000
+	}
+	fmt.Println(ans)
 }
--- ddcc2020-qual/b/main.go
+++ ddcc2020-qual/b/main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main() {
+	n := scanInt()
+	a := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		x := scanInt()
+		a[i] = a[i-1] + x
+	}
+
+	ans := math.MaxInt64
+	for i := 1; i < n; i++ {
+		ans = min(ans, abs(a[len(a)-1]-a[i]*2))
+	}
+	fmt.Println(ans)
 }
