d98fead9ef2dbf630276c93ff9e960575309cfdf
matsutake <matsutake.pg@gmail.com>
2020-05-11 09:03:38 +0900

    式変形と考察

--- diverta2019/d/main.go
+++ diverta2019/d/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	ans := 0
+	for i := 1; i*i < n; i++ {
+		if n%i == 0 {
+			if m := n/i - 1; m > i {
+				ans += m
+			}
+		}
+	}
+	fmt.Println(ans)
 }
