35a2aa4b086feb6bc0056de0816f4056d800e2a1
matsutake <matsutake.pg@gmail.com>
2020-05-15 05:48:16 +0900

    分布を持って数え上げ

--- sumitrust2019/e/main.go
+++ sumitrust2019/e/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n := scanInt()
+	const mod = 1000000007
+	xm := make(map[int]int)
+	xm[0] = 3
+	ans := 1
+	for i := 0; i < n; i++ {
+		a := scanInt()
+		ans *= xm[a]
+		ans %= mod
+		xm[a]--
+		xm[a+1]++
+	}
+	fmt.Println(ans)
 }
