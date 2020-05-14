e1de2d75e2fad97c940fece5287cb46265c64b17
matsutake <matsutake.pg@gmail.com>
2020-05-15 02:22:51 +0900

    答えのパターンが少なければ全探索

--- sumitrust2019/d/main.go
+++ sumitrust2019/d/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var (
+		n int
+		s string
+	)
+	fmt.Scan(&n, &s)
+
+	ans := 0
+	for i := 0; i <= 999; i++ {
+		x := fmt.Sprintf("%03d", i)
+		p := 0
+		for xi := range x {
+			found := false
+			for si := p; si < n; si++ {
+				if x[xi] == s[si] {
+					found = true
+					p = si + 1
+					break
+				}
+			}
+			if !found {
+				break
+			} else if xi == 2 && found {
+				ans++
+			}
+		}
+	}
+	fmt.Println(ans)
 }
