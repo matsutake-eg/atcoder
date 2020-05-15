fd1637712b15ffc62464d0491fc5cddbd632c578
matsutake <matsutake.pg@gmail.com>
2020-05-16 06:01:07 +0900

    過去問

--- exawizards2019/b/main.go
+++ exawizards2019/b/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	var (
+		n int
+		s string
+	)
+	fmt.Scan(&n, &s)
+
+	r := strings.Count(s, "R")
+	b := n - r
+	if r > b {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
 }
