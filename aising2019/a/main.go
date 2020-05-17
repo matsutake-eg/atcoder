9caa11e545e484d05f02e15ac28ddc2ff9be514a
matsutake <matsutake.pg@gmail.com>
2020-05-18 01:47:45 +0900

    回答

--- aising2019/a/main.go
+++ aising2019/a/main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n, h, w int
+	fmt.Scan(&n, &h, &w)
+
+	fmt.Println((n - h + 1) * (n - w + 1))
 }
--- aising2019/b/main.go
+++ aising2019/b/main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func mins(x ...int) int {
+	mv := x[0]
+	for _, v := range x[1:] {
+		if v < mv {
+			mv = v
+		}
+	}
+	return mv
+}
+
+func main() {
+	n, a, b := scanInt(), scanInt(), scanInt()
+	ca, cb, cc := 0, 0, 0
+	for i := 0; i < n; i++ {
+		p := scanInt()
+		if p <= a {
+			ca++
+		} else if p >= b+1 {
+			cc++
+		} else {
+			cb++
+		}
+	}
+	fmt.Println(mins(ca, cb, cc))
 }
