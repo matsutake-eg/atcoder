90735ba7c16fb13e648cdcc9bcdca18971a88b70
matsutake <matsutake.pg@gmail.com>
2020-05-18 01:21:32 +0900

    時計

--- abc168/c/main.go
+++ abc168/c/main.go
@@ -5,24 +5,27 @@ import (
 	"math"
 )
 
+func abs(x float64) float64 {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func min(x, y float64) float64 {
+	if x < y {
+		return x
+	}
+	return y
+}
+
 func main() {
 	var a, b, h, m float64
 	fmt.Scan(&a, &b, &h, &m)
 
-	if h >= 12 {
-		h -= 12
-	}
-
-	l := 360 * (float64(m) / 60)
-	s := 30*float64(h) + 30*(float64(m)/60)
-	var ang float64
-	if l > s {
-		ang = l - s
-	} else {
-		ang = s - l
-	}
-	if ang >= 180 {
-		ang = 360 - ang
-	}
+	hang := 30*h + 30*(m/60)
+	mang := 360 * (m / 60)
+	ang := abs(hang - mang)
+	ang = min(ang, 360-ang)
 	fmt.Println(math.Sqrt(a*a + b*b - 2*a*b*math.Cos(math.Pi*ang/180)))
 }
