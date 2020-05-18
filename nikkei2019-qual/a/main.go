b68afdb60bdba3977999068e4e2e8265e807c846
matsutake <matsutake.pg@gmail.com>
2020-05-19 06:51:38 +0900

    積の最大・最小

--- nikkei2019-qual/a/main.go
+++ nikkei2019-qual/a/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	var n, a, b int
+	fmt.Scan(&n, &a, &b)
+
+	fmt.Println(min(a, b), max(0, a-(n-b)))
 }
