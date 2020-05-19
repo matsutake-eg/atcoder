33d886a9b28faec77f80144a1258d56452f95469
matsutake <matsutake.pg@gmail.com>
2020-05-20 08:15:14 +0900

    (自分の利益-相手の利益)を最大化

--- nikkei2019-qual/b/main.go
+++ nikkei2019-qual/b/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var (
+		n       int
+		a, b, c string
+	)
+	fmt.Scan(&n, &a, &b, &c)
+
+	ans := 0
+	for i := 0; i < n; i++ {
+		if a[i] != b[i] && b[i] != c[i] && c[i] != a[i] {
+			ans += 2
+		} else if !(a[i] == b[i] && b[i] == c[i] && c[i] == a[i]) {
+			ans++
+		}
+	}
+	fmt.Println(ans)
 }
--- nikkei2019-qual/c/main.go
+++ nikkei2019-qual/c/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n := scanInt()
+	sum := make([]int, n)
+	ans := 0
+	for i := range sum {
+		a, b := scanInt(), scanInt()
+		sum[i] = a + b
+		ans += -b
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(sum)))
+
+	for i := 0; i < n; i += 2 {
+		ans += sum[i]
+	}
+	fmt.Println(ans)
 }
