e7fd1acb091e4b0f224224a38b550659460b059c
matsutake <matsutake.pg@gmail.com>
2020-05-21 05:33:34 +0900

    部分文字列全探索

--- keyence2019/a/main.go
+++ keyence2019/a/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	xm := make(map[int]bool)
+	for i := 0; i < 4; i++ {
+		var n int
+		fmt.Scan(&n)
+		xm[n] = true
+	}
+
+	if xm[1] && xm[9] && xm[7] && xm[4] {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
 }
--- keyence2019/b/main.go
+++ keyence2019/b/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var s string
+	fmt.Scan(&s)
+
+	if s == "keyence" {
+		fmt.Println("YES")
+		return
+	}
+
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j < len(s); j++ {
+			if s[:i]+s[j:] == "keyence" {
+				fmt.Println("YES")
+				return
+			}
+		}
+	}
+	fmt.Println("NO")
 }
