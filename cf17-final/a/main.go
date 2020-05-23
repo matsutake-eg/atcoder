0e9907f442441fd6abf24b852143fcd518d428c0
matsutake <matsutake.pg@gmail.com>
2020-05-23 12:36:20 +0900

    数える

--- cf17-final/a/main.go
+++ cf17-final/a/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var s string
+	fmt.Scan(&s)
+
+	switch s {
+	case "AKIHABARA", "KIHABARA", "AKIHBARA", "AKIHABRA", "AKIHABAR", "KIHBARA", "KIHABRA", "KIHABAR", "AKIHBRA", "AKIHBAR", "AKIHABR", "KIHBRA", "KIHBAR", "KIHABR", "AKIHBR", "KIHBR":
+		fmt.Println("YES")
+	default:
+		fmt.Println("NO")
+	}
 }
