cc41e8d8036692cb3fe95e9708a6acd5e26762a3
matsutake <matsutake.pg@gmail.com>
2020-05-23 12:54:45 +0900

    回文

--- cf17-final/b/main.go
+++ cf17-final/b/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import "fmt"
 
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main() {
+	var s string
+	fmt.Scan(&s)
+
+	ca, cb, cc := 0, 0, 0
+	for _, r := range s {
+		switch r {
+		case 'a':
+			ca++
+		case 'b':
+			cb++
+		case 'c':
+			cc++
+		}
+	}
+	if abs(ca-cb) > 1 || abs(cb-cc) > 1 || abs(cc-ca) > 1 {
+		fmt.Println("NO")
+	} else {
+		fmt.Println("YES")
+	}
 }
