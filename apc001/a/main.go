e6d927b42c13fe49063afa6a219d8184d3eb80da
matsutake <matsutake.pg@gmail.com>
2020-05-24 08:44:29 +0900

    数え上げ

--- apc001/a/main.go
+++ apc001/a/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var x, y int
+	fmt.Scan(&x, &y)
+
+	if x%y == 0 {
+		fmt.Println(-1)
+	} else {
+		fmt.Println(x)
+	}
 }
--- apc001/b/main.go
+++ apc001/b/main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n := scanInt()
+	a := make([]int, n)
+	for i := range a {
+		a[i] = scanInt()
+	}
+	b := make([]int, n)
+	for i := range b {
+		b[i] = scanInt()
+	}
+
+	d := 0
+	cnt := 0
+	for i := 0; i < n; i++ {
+		d += b[i] - a[i]
+		if a[i] < b[i] {
+			cnt += (b[i] - a[i] + 1) / 2
+		}
+	}
+	if d >= cnt {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
 }
