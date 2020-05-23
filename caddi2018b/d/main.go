9ffe1279d62edb79beeba503281993aafe13366c
matsutake <matsutake.pg@gmail.com>
2020-05-23 09:05:41 +0900

    状態と偶奇

--- caddi2018b/d/main.go
+++ caddi2018b/d/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n := scanInt()
+	for i := 0; i < n; i++ {
+		a := scanInt()
+		if a%2 == 1 {
+			fmt.Println("first")
+			return
+		}
+	}
+	fmt.Println("second")
 }
