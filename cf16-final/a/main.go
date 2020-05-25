9d7646c45d1184d00b57871210060aacea46af62
matsutake <matsutake.pg@gmail.com>
2020-05-26 08:42:28 +0900

    下から埋める

--- cf16-final/a/main.go
+++ cf16-final/a/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	iv, _ := strconv.Atoi(scanString())
+	return iv
+}
+
+func scanString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 100001), 100001*100)
+}
+
+func main() {
+	h, w := scanInt(), scanInt()
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			s := scanString()
+			if s == "snuke" {
+				fmt.Println(string('A'+j) + strconv.Itoa(i+1))
+				return
+			}
+		}
+	}
 }
--- cf16-final/b/main.go
+++ cf16-final/b/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	sum := 0
+	wr := bufio.NewWriter(os.Stdout)
+	for i := 1; ; i++ {
+		sum += i
+		if d := sum - n; d > 0 {
+			for j := 1; j <= i; j++ {
+				if j == d {
+					continue
+				}
+				wr.WriteString(strconv.Itoa(j) + "\n")
+			}
+			wr.Flush()
+			return
+		}
+	}
 }
