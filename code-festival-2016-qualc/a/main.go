09f6be34d8131e672b65675a95e3dc963d491557
matsutake <matsutake.pg@gmail.com>
2020-05-27 08:43:27 +0900

    大きいものから並べて考える

--- code-festival-2016-qualc/a/main.go
+++ code-festival-2016-qualc/a/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var s string
+	fmt.Scan(&s)
+
+	foundC := false
+	for _, r := range s {
+		if foundC && r == 'F' {
+			fmt.Println("Yes")
+			return
+		} else if r == 'C' {
+			foundC = true
+		}
+	}
+	fmt.Println("No")
 }
