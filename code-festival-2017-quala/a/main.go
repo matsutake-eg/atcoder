c844cb741ecc47674f3b771d643318e92813e4fd
matsutake <matsutake.pg@gmail.com>
2020-05-29 08:32:15 +0900

    考察してから全探索

--- code-festival-2017-quala/a/main.go
+++ code-festival-2017-quala/a/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var s string
+	fmt.Scan(&s)
+
+	if len(s) >= 4 && s[:4] == "YAKI" {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
 }
--- code-festival-2017-quala/b/main.go
+++ code-festival-2017-quala/b/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n, m, k int
+	fmt.Scan(&n, &m, &k)
+
+	for i := 0; i <= n; i++ {
+		for j := 0; j <= m; j++ {
+			if n*j+m*i-2*i*j == k {
+				fmt.Println("Yes")
+				return
+			}
+		}
+	}
+	fmt.Println("No")
 }
