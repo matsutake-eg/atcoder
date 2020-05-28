ca950a6410f9e58f31bb832f5b91774caf27b839
matsutake <matsutake.pg@gmail.com>
2020-05-28 09:07:16 +0900

    回答

--- code-festival-2017-qualb/b/main.go
+++ code-festival-2017-qualb/b/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n := scanInt()
+	dm := make(map[int]int)
+	for i := 0; i < n; i++ {
+		d := scanInt()
+		dm[d]++
+	}
+	m := scanInt()
+	for i := 0; i < m; i++ {
+		t := scanInt()
+		if dm[t] == 0 {
+			fmt.Println("NO")
+			return
+		}
+		dm[t]--
+	}
+	fmt.Println("YES")
 }
