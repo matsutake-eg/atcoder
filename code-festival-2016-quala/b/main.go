5ed999fba256610c0dfe2a50be94669d7b4836f2
matsutake <matsutake.pg@gmail.com>
2020-05-28 09:44:16 +0900

    回答

--- code-festival-2016-quala/b/main.go
+++ code-festival-2016-quala/b/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n := scanInt()
+	a := make([]int, n)
+	for i := range a {
+		a[i] = scanInt()
+	}
+
+	ans := 0
+	for i := range a {
+		if i+1 == a[a[i]-1] {
+			ans++
+		}
+	}
+	fmt.Println(ans / 2)
 }
--- code-festival-2016-quala/c/main.go
+++ code-festival-2016-quala/c/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	var (
+		s string
+		k int
+	)
+	fmt.Scan(&s, &k)
+
+	wr := bufio.NewWriter(os.Stdout)
+	for i := range s {
+		d := int(s[i] - 'a')
+		if i == len(s)-1 {
+			wr.WriteString(string('a' + (d+k)%26))
+			break
+		}
+
+		if d > 0 && 26-d <= k {
+			wr.WriteString("a")
+			k -= 26 - d
+		} else {
+			wr.WriteString(string(s[i]))
+		}
+	}
+	wr.WriteString("\n")
+	wr.Flush()
 }
