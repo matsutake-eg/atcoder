644e7809a527c8894ab18b5b2a90a87d27e7c5d7
matsutake <matsutake.pg@gmail.com>
2020-06-01 04:24:48 +0900

    色水

--- agc002/a/main.go
+++ agc002/a/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+
+	if a <= 0 && b >= 0 {
+		fmt.Println("Zero")
+	} else if a > 0 || (b-a+1)%2 == 0 {
+		fmt.Println("Positive")
+	} else {
+		fmt.Println("Negative")
+	}
 }
--- agc002/b/main.go
+++ agc002/b/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n, m := scanInt(), scanInt()
+	a := make([]int, n+1)
+	b := make([]bool, n+1)
+	for i := range a {
+		a[i] = 1
+	}
+	b[1] = true
+	for i := 0; i < m; i++ {
+		x, y := scanInt(), scanInt()
+		if b[x] {
+			if a[x] == 1 {
+				b[x] = false
+			}
+			b[y] = true
+		}
+		a[x]--
+		a[y]++
+	}
+	ans := 0
+	for _, v := range b[1:] {
+		if v {
+			ans++
+		}
+	}
+	fmt.Println(ans)
 }
