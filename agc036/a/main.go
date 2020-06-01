307dbf1ccd6176a8f576db4c2ac5cbc2225c798f
matsutake <matsutake.pg@gmail.com>
2020-06-02 07:42:59 +0900

    三角形の面積難しい版

--- agc036/a/main.go
+++ agc036/a/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var s int
+	fmt.Scan(&s)
+
+	v := 1000000000
+	x := (v - s%v) % v
+	y := (s + x) / v
+	fmt.Println(0, 0, v, 1, x, y)
 }
