374e37a7574e3b04b7e4878c66c18ae670509491
matsutake <matsutake.pg@gmail.com>
2020-06-04 08:16:25 +0900

    べき乗

--- agc025/a/main.go
+++ agc025/a/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
 
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	ans := math.MaxInt64
+	for i := 1; i <= n/2; i++ {
+		sum := 0
+		for _, r := range strconv.Itoa(i) {
+			sum += int(r - '0')
+		}
+		for _, r := range strconv.Itoa(n - i) {
+			sum += int(r - '0')
+		}
+		ans = min(ans, sum)
+	}
+	fmt.Println(ans)
 }
