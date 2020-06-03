0333685ed67f69f8801d3ce5fbe9e5f984ca805e
matsutake <matsutake.pg@gmail.com>
2020-06-04 08:42:11 +0900

    回答

--- agc024/a/main.go
+++ agc024/a/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a, b, c, k int
+	fmt.Scan(&a, &b, &c, &k)
+
+	if k%2 == 1 {
+		fmt.Println(b - a)
+	} else {
+		fmt.Println(a - b)
+	}
 }
