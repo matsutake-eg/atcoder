046513fe811fae0ed174ada108a343bd863ad701
matsutake <matsutake.pg@gmail.com>
2020-06-03 17:04:03 +0900

    回答

--- agc020/a/main.go
+++ agc020/a/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n, a, b int
+	fmt.Scan(&n, &a, &b)
+
+	for {
+		if a+1 < b {
+			a++
+		} else if a-1 >= 1 {
+			a--
+		}
+		if b-1 > a {
+			b--
+		} else if b+1 <= n {
+			b++
+		}
+
+		if a == 1 {
+			fmt.Println("Borys")
+			return
+		}
+		if b == n {
+			fmt.Println("Alice")
+			return
+		}
+	}
 }
