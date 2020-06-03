158996ab459b90ab2fc6afa7649f9ba33d1db351
matsutake <matsutake.pg@gmail.com>
2020-06-04 07:52:34 +0900

    回答

--- agc027/a/main.go
+++ agc027/a/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n, x := scanInt(), scanInt()
+	a := make([]int, n)
+	for i := range a {
+		a[i] = scanInt()
+	}
+	sort.Ints(a)
+
+	for i := range a {
+		x -= a[i]
+		if x < 0 {
+			fmt.Println(i)
+			return
+		}
+	}
+	if x == 0 {
+		fmt.Println(n)
+	} else {
+		fmt.Println(n - 1)
+	}
 }
