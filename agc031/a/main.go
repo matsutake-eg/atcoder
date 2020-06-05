9c0e5084456290a9af32831d3ccb80beaf6adfbd
matsutake <matsutake.pg@gmail.com>
2020-06-05 09:19:15 +0900

    ユニーク部分列

--- agc031/a/main.go
+++ agc031/a/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var (
+		n int
+		s string
+	)
+	fmt.Scan(&n, &s)
+
+	xm := make(map[rune]int)
+	for _, r := range s {
+		xm[r]++
+	}
+
+	const mod = 1000000007
+	ans := 1
+	for _, v := range xm {
+		ans *= v + 1
+		ans %= mod
+	}
+	fmt.Println(ans - 1)
 }
