07000ba424e96857dbe8b38f50a5cc54f61e9e54
matsutake <matsutake.pg@gmail.com>
2020-06-08 06:16:59 +0900

    サンプルはあくまで一例

--- agc021/a/main.go
+++ agc021/a/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	s := strconv.Itoa(n)
+	c, _ := strconv.Atoi(string(s[0]))
+	for i := 1; i < len(s); i++ {
+		if s[i] != '9' {
+			fmt.Println(c - 1 + (len(s)-1)*9)
+			return
+		}
+	}
+	fmt.Println(c + (len(s)-1)*9)
 }
