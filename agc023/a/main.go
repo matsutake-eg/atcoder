b63660ebd1b4fff34f0c052e0733415d8060c3c4
matsutake <matsutake.pg@gmail.com>
2020-06-08 00:22:20 +0900

    数列の連続部分の和が0

--- agc023/a/main.go
+++ agc023/a/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n := scanInt()
+	sum := 0
+	xm := make(map[int]int)
+	xm[0]++
+	for i := 0; i < n; i++ {
+		a := scanInt()
+		sum += a
+		xm[sum]++
+	}
+
+	ans := 0
+	for _, v := range xm {
+		ans += (v * (v - 1)) / 2
+	}
+	fmt.Println(ans)
 }
