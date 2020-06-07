1dad6e095f15c7237cce5ee57d65c9fca2b4ef7d
matsutake <matsutake.pg@gmail.com>
2020-06-08 06:06:20 +0900

    回答

--- agc019/a/main.go
+++ agc019/a/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func mins(x ...int) int {
+	mv := x[0]
+	for _, v := range x[1:] {
+		if v < mv {
+			mv = v
+		}
+	}
+	return mv
+}
+
+func main() {
+	var q, h, s, d, n int
+	fmt.Scan(&q, &h, &s, &d, &n)
+
+	x := mins(q*4, h*2, s)
+	if x*2 < d {
+		fmt.Println(x * n)
+	} else {
+		fmt.Println(d*(n/2) + x*(n%2))
+	}
 }
