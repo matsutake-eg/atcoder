6f1eecba1f7b902d90ba878fa810dc0ded94c4b7
matsutake <matsutake.pg@gmail.com>
2020-06-07 16:34:31 +0900

    回答

--- agc030/a/main.go
+++ agc030/a/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a, b, c int
+	fmt.Scan(&a, &b, &c)
+
+	if c <= a+b {
+		fmt.Println(b + c)
+	} else {
+		fmt.Println(a + b*2 + 1)
+	}
 }
