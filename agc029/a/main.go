a0819a93685a886d55618a6b846f00a2db6c2f66
matsutake <matsutake.pg@gmail.com>
2020-06-07 16:40:34 +0900

    回答

--- agc029/a/main.go
+++ agc029/a/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var s string
+	fmt.Scan(&s)
+
+	cnt := 0
+	ans := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == 'W' {
+			cnt++
+		} else {
+			ans += cnt
+		}
+	}
+	fmt.Println(ans)
 }
