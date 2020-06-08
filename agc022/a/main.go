a6486f34d54c83d41a44630a6dbded610f530c85
matsutake <matsutake.pg@gmail.com>
2020-06-08 21:49:59 +0900

    辞書地獄

--- agc022/a/main.go
+++ agc022/a/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	var s string
+	fmt.Scan(&s)
+
+	az := "abcdefghijklmnopqrstuvwxyz"
+	if len(s) < 26 {
+		for _, r := range az {
+			if !strings.ContainsRune(s, r) {
+				fmt.Println(s + string(r))
+				return
+			}
+		}
+	} else {
+		for i := len(s) - 1; i >= 1; i-- {
+			if s[i-1] < s[i] {
+				for _, r := range az {
+					if r > rune(s[i-1]) && !strings.ContainsRune(s[:i], r) {
+						fmt.Println(s[:i-1] + string(r))
+						return
+					}
+				}
+			}
+		}
+		fmt.Println(-1)
+	}
 }
