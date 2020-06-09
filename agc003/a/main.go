849e998bd85a37878f44711da5da28133a35ca78
matsutake <matsutake.pg@gmail.com>
2020-06-09 20:11:54 +0900

    回答

--- agc003/a/main.go
+++ agc003/a/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var s string
+	fmt.Scan(&s)
+
+	xm := make(map[rune]bool)
+	for _, r := range s {
+		xm[r] = true
+	}
+	if xm['N'] == xm['S'] && xm['E'] == xm['W'] {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
 }
--- agc003/b/main.go
+++ agc003/b/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n := scanInt()
+	x := 0
+	ans := 0
+	for i := 0; i < n; i++ {
+		a := scanInt()
+		if a > 0 {
+			if x > a {
+				ans += a
+				x = 0
+			} else {
+				ans += x
+				x = a - x
+				if x >= 2 {
+					ans += x / 2
+					x %= 2
+				}
+			}
+		} else {
+			x = 0
+		}
+	}
+	fmt.Println(ans)
 }
