c41e02eefd428d8b12b2bc49cfb015082c945260
matsutake <matsutake.pg@gmail.com>
2020-06-09 21:11:35 +0900

    手順をパターン化して全探索

--- agc008/a/main.go
+++ agc008/a/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	var x, y int
+	fmt.Scan(&x, &y)
+
+	ans := math.MaxInt64
+	if x <= y {
+		ans = min(ans, y-x)
+	}
+	if a := -x; a <= y {
+		ans = min(ans, y-a+1)
+	}
+	if b := -y; x <= b {
+		ans = min(ans, b-x+1)
+	}
+	if a, b := -x, -y; a <= b {
+		ans = min(ans, b-a+2)
+	}
+	fmt.Println(ans)
 }
