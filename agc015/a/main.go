bf878bf9d771b1f2975ca0672a90b05f6a7162bc
matsutake <matsutake.pg@gmail.com>
2020-06-09 20:40:38 +0900

    回答

--- agc015/a/main.go
+++ agc015/a/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	var n, a, b int
+	fmt.Scan(&n, &a, &b)
+
+	fmt.Println(max(0, a+b*(n-1)-(b+a*(n-1))+1))
 }
--- agc015/b/main.go
+++ agc015/b/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var s string
+	fmt.Scan(&s)
+
+	ans := 0
+	for i := range s {
+		u := len(s) - i - 1
+		d := i
+		if s[i] == 'U' {
+			ans += u + d*2
+		} else {
+			ans += u*2 + d
+		}
+	}
+	fmt.Println(ans)
 }
