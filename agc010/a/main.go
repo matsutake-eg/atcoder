bb081c3e4c417fa540f9db9d177128130ceda025
matsutake <matsutake.pg@gmail.com>
2020-06-10 20:34:39 +0900

    回答

--- agc010/a/main.go
+++ agc010/a/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n := scanInt()
+	cnt := 0
+	for i := 0; i < n; i++ {
+		a := scanInt()
+		if a%2 == 1 {
+			cnt++
+		}
+	}
+	if cnt%2 == 0 {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
 }
