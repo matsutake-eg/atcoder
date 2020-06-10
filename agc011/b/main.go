6d30f9c3d2c5fb7a397c80328abc7d065e7a8cf6
matsutake <matsutake.pg@gmail.com>
2020-06-10 21:52:31 +0900

    怪獣合体

--- agc011/b/main.go
+++ agc011/b/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n := scanInt()
+	a := make([]int, n)
+	for i := range a {
+		a[i] = scanInt()
+	}
+	sort.Ints(a)
+
+	sum := 0
+	ans := 0
+	for i := 0; i < n-1; i++ {
+		ans++
+		sum += a[i]
+		if sum*2 < a[i+1] {
+			ans = 0
+		}
+	}
+	fmt.Println(ans + 1)
 }
