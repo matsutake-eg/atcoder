6ef7ce6f7fb7f9e1b128fa90a44599b30d2c5ade
matsutake <matsutake.pg@gmail.com>
2020-06-10 21:09:34 +0900

    空港バス

--- agc011/a/main.go
+++ agc011/a/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n, c, k := scanInt(), scanInt(), scanInt()
+	t := make([]int, n)
+	for i := range t {
+		t[i] = scanInt()
+	}
+	sort.Ints(t)
+
+	ans := 1
+	l := t[0] + k
+	x := 1
+	for i := 1; i < n; i++ {
+		x++
+		if x > c || t[i] > l {
+			ans++
+			l = t[i] + k
+			x = 1
+		}
+	}
+	fmt.Println(ans)
 }
