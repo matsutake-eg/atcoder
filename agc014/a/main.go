34a78d9ecaa254c54d1eb6f735ddabc4b892ff3d
matsutake <matsutake.pg@gmail.com>
2020-06-12 23:15:39 +0900

    回答

--- agc014/a/main.go
+++ agc014/a/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	var a, b, c int
+	fmt.Scan(&a, &b, &c)
+
+	if a%2 == 1 || b%2 == 1 || c%2 == 1 {
+		fmt.Println(0)
+		return
+	} else if a == b && b == c && c == a {
+		fmt.Println(-1)
+		return
+	}
+
+	ans := math.MaxInt64
+	for _, v := range []int{a + b, b + c, c + a} {
+		cnt := 0
+		for v%2 == 0 {
+			v /= 2
+			cnt++
+		}
+		ans = min(ans, cnt)
+	}
+	fmt.Println(ans)
 }
--- agc014/b/main.go
+++ agc014/b/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	_, m := scanInt(), scanInt()
+	xm := make(map[int]int)
+	for i := 0; i < m; i++ {
+		a, b := scanInt(), scanInt()
+		xm[a]++
+		xm[b]++
+	}
+
+	for _, v := range xm {
+		if v%2 == 1 {
+			fmt.Println("NO")
+			return
+		}
+	}
+	fmt.Println("YES")
 }
