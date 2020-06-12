1607b5942db9284c287da5159d53645e06ff775b
matsutake <matsutake.pg@gmail.com>
2020-06-12 23:25:25 +0900

    回答

--- agc005/a/main.go
+++ agc005/a/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var x string
+	fmt.Scan(&x)
+
+	ans := len(x)
+	cnt := 0
+	for _, r := range x {
+		if r == 'S' {
+			cnt++
+		} else if r == 'T' && cnt > 0 {
+			cnt--
+			ans -= 2
+		}
+	}
+	fmt.Println(ans)
 }
