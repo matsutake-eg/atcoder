03517d609623b500792d3cc610ff0914522487ba
matsutake <matsutake.pg@gmail.com>
2020-06-12 09:06:23 +0900

    回答

--- agc006/a/main.go
+++ agc006/a/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var (
+		n    int
+		s, t string
+	)
+	fmt.Scan(&n, &s, &t)
+
+	if s == t {
+		fmt.Println(len(s))
+		return
+	}
+
+	ans := s + t
+	for i := 1; i < n; i++ {
+		if s[len(s)-i:] == t[:i] {
+			ans = s + t[i:]
+		}
+	}
+	fmt.Println(len(ans))
 }
