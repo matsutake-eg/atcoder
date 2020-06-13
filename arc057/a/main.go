ea1cdbc1d26603d3915177b46543499697d4167a
matsutake <matsutake.pg@gmail.com>
2020-06-13 13:40:43 +0900

    落ち着いて解く

--- arc057/a/main.go
+++ arc057/a/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a, k int
+	fmt.Scan(&a, &k)
+
+	const limit = 2000000000000
+	if k == 0 {
+		fmt.Println(limit - a)
+		return
+	}
+
+	ans := 0
+	for {
+		a += 1 + k*a
+		ans++
+		if 1 <= a/limit {
+			fmt.Println(ans)
+			return
+		}
+	}
 }
