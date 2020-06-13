cc9b02309da2b1d0bdf7da5c7c6622f167425fdf
matsutake <matsutake.pg@gmail.com>
2020-06-13 14:16:25 +0900

    時刻処理

--- arc002/a/main.go
+++ arc002/a/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var y int
+	fmt.Scan(&y)
+
+	if y%400 == 0 {
+		fmt.Println("YES")
+		return
+	}
+	if y%100 == 0 {
+		fmt.Println("NO")
+		return
+	}
+	if y%4 == 0 {
+		fmt.Println("YES")
+		return
+	}
+	fmt.Println("NO")
 }
--- arc002/b/main.go
+++ arc002/b/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
 
+func main() {
+	var s string
+	fmt.Scan(&s)
+
+	x := strings.Split(s, "/")
+	y, _ := strconv.Atoi(x[0])
+	m, _ := strconv.Atoi(x[1])
+	d, _ := strconv.Atoi(x[2])
+	t := time.Date(y, time.Month(m), d, 0, 0, 0, 0, time.UTC)
+	for {
+		y, m, d := t.Year(), int(t.Month()), t.Day()
+		if y%(m*d) == 0 {
+			fmt.Printf("%02v/%02v/%02v\n", y, m, d)
+			return
+		}
+		t = t.AddDate(0, 0, 1)
+	}
 }
