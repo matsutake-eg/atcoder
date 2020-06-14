ce6d70b2acb8c07c1c885a8fc54eca0d266e7aab
matsutake <matsutake.pg@gmail.com>
2020-06-14 20:15:46 +0900

    回答

--- arc003/a/main.go
+++ arc003/a/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	iv, _ := strconv.Atoi(scanString())
+	return iv
+}
+
+func scanString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 100001), 100001*100)
+}
+
+func main() {
+	n := float64(scanInt())
+	r := scanString()
+	ans := 0.0
+	for _, v := range r {
+		switch v {
+		case 'A':
+			ans += 4
+		case 'B':
+			ans += 3
+		case 'C':
+			ans += 2
+		case 'D':
+			ans++
+		}
+	}
+	fmt.Println(ans / n)
 }
--- arc003/b/main.go
+++ arc003/b/main.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
+	"sort"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	iv, _ := strconv.Atoi(scanString())
+	return iv
+}
+
+func scanString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 100001), 100001*100)
+}
+
+func main() {
+	n := scanInt()
+	s := make([]string, n)
+	for i := range s {
+		s[i] = scanString()
+	}
+
+	rs := make([]string, n)
+	for i := range s {
+		r := []rune(s[i])
+		for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+			r[i], r[j] = r[j], r[i]
+		}
+		rs[i] = string(r)
+	}
+
+	sort.Strings(rs)
+	wr := bufio.NewWriter(os.Stdout)
+	for i := range rs {
+		r := []rune(rs[i])
+		for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+			r[i], r[j] = r[j], r[i]
+		}
+		wr.WriteString(string(r) + "\n")
+	}
+	wr.Flush()
 }
