20764587354cfccbd99c1aca07eedafe16082ff4
matsutake <matsutake.pg@gmail.com>
2020-06-16 05:06:18 +0900

    考察

--- arc004/b/main.go
+++ arc004/b/main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	n := scanInt()
+	d := make([]int, n)
+	mx := 0
+	l := 0
+	for i := range d {
+		d[i] = scanInt()
+		mx += d[i]
+		l = max(l, d[i])
+	}
+
+	fmt.Println(mx)
+	fmt.Println(max(0, l-(mx-l)))
 }
