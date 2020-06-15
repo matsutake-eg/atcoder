c53474c1163b480a3fbbd652640434bd8280bf9e
matsutake <matsutake.pg@gmail.com>
2020-06-15 09:22:51 +0900

    回答

--- arc004/a/main.go
+++ arc004/a/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func max(x, y float64) float64 {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	n := scanInt()
+	xs := make([]float64, n)
+	ys := make([]float64, n)
+	for i := 0; i < n; i++ {
+		xs[i], ys[i] = float64(scanInt()), float64(scanInt())
+	}
+	ans := 0.0
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			d := math.Sqrt(math.Pow(xs[i]-xs[j], 2) + math.Pow(ys[i]-ys[j], 2))
+			ans = max(ans, d)
+		}
+	}
+	fmt.Println(ans)
 }
--- arc005/a/main.go
+++ arc005/a/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	iv, _ := strconv.Atoi(scanString())
+	return iv
+}
+
+func scanString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 100001), 100001*100)
+}
+
+func main() {
+	n := scanInt()
+	xm := make(map[string]bool)
+	xm["TAKAHASHIKUN"] = true
+	xm["Takahashikun"] = true
+	xm["takahashikun"] = true
+	ans := 0
+	for i := 0; i < n; i++ {
+		w := scanString()
+		if i == n-1 {
+			w = w[:len(w)-1]
+		}
+		if xm[w] {
+			ans++
+		}
+	}
+	fmt.Println(ans)
 }
