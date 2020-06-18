865a0e027b6b099f60e4d2cb58e4d7166a3551e4
matsutake <matsutake.pg@gmail.com>
2020-06-18 20:24:22 +0900

    回答

--- tenka1-2012-qualA/a/main.go
+++ tenka1-2012-qualA/a/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	xs := make([]int, 46)
+	xs[0] = 1
+	xs[1] = 1
+	for i := 2; i < len(xs); i++ {
+		xs[i] = xs[i-1] + xs[i-2]
+	}
+	fmt.Println(xs[n])
 }
--- tenka1-2012-qualA/b/main.go
+++ tenka1-2012-qualA/b/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func init() {
+	sc.Split(bufio.ScanLines)
+	sc.Buffer(make([]byte, 100001), 100001*100)
+}
+
+func main() {
+	c := scanString()
+	wr := bufio.NewWriter(os.Stdout)
+	sp := false
+	for _, r := range c {
+		if sp && r != ' ' {
+			sp = false
+			wr.WriteString("," + string(r))
+		} else if !sp && r == ' ' {
+			sp = true
+		} else if !sp && r != ' ' {
+			wr.WriteString(string(r))
+		}
+	}
+	wr.WriteString("\n")
+	wr.Flush()
 }
