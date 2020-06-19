8d3019412589b22338e9fa5e17ec8532793ef163
matsutake <matsutake.pg@gmail.com>
2020-06-20 08:46:09 +0900

    落ち着いて解け

--- discovery2016-qual/a/main.go
+++ discovery2016-qual/a/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	var w int
+	fmt.Scan(&w)
+
+	s := "DiscoPresentsDiscoveryChannelProgrammingContest2016"
+	for i := 0; i < len(s); i += w {
+		fmt.Println(s[i:min(len(s), i+w)])
+	}
 }
