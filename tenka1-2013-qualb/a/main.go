1563ca17e994070c323f2d73b702647fde647631
matsutake <matsutake.pg@gmail.com>
2020-06-19 21:30:40 +0900

    回答

--- tenka1-2013-qualb/a/main.go
+++ tenka1-2013-qualb/a/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 100001), 100001*100)
+}
+
+func main() {
+	ss := make([]string, 15)
+	for i := range ss {
+		ss[i] = scanString()
+	}
+	sort.Strings(ss)
+	fmt.Println(ss[6])
 }
