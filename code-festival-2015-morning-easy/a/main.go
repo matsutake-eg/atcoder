8e027ea72ccde531b22e990f8c86a4a1c3d45cd7
matsutake <matsutake.pg@gmail.com>
2020-06-19 22:26:44 +0900

    配列外参照地獄

--- code-festival-2015-morning-easy/a/main.go
+++ code-festival-2015-morning-easy/a/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n int
+	fmt.Scan(&n)
+	for i := 1; ; i++ {
+		if v := i * i; v >= n {
+			fmt.Println(v - n)
+			return
+		}
+	}
 }
--- code-festival-2015-morning-easy/b/main.go
+++ code-festival-2015-morning-easy/b/main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n, k, m, r := scanInt(), scanInt(), scanInt(), scanInt()
+	ss := make([]int, n-1)
+	for i := range ss {
+		ss[i] = scanInt()
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(ss)))
+
+	sum := 0
+	for i := 0; i < k-1; i++ {
+		sum += ss[i]
+	}
+
+	if v := r*k - sum; v > m {
+		fmt.Println(-1)
+	} else if k < n {
+		if ss[k-1] >= v {
+			fmt.Println(0)
+		} else {
+			fmt.Println(v)
+		}
+	} else {
+		if v <= 0 {
+			fmt.Println(0)
+		} else {
+			fmt.Println(v)
+		}
+	}
 }
