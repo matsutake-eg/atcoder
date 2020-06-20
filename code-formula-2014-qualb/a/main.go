b9b15d8cd06e7b6834b02c1eb308c1246a2171b5
matsutake <matsutake.pg@gmail.com>
2020-06-20 16:41:21 +0900

    回答

--- code-formula-2014-qualb/a/main.go
+++ code-formula-2014-qualb/a/main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	fmt.Println(7 - n)
 }
--- code-formula-2014-qualb/b/main.go
+++ code-formula-2014-qualb/b/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 100001), 100001*100)
+}
+
+func main() {
+	n := scanString()
+	o, e := 0, 0
+	cnt := 1
+	for i := len(n) - 1; i >= 0; i-- {
+		x := int(n[i] - '0')
+		if cnt%2 == 1 {
+			o += x
+		} else {
+			e += x
+		}
+		cnt++
+	}
+	fmt.Println(e, o)
 }
