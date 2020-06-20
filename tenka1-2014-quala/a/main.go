7c2188473d5912915724f6c10c25a86901fca41c
matsutake <matsutake.pg@gmail.com>
2020-06-20 11:24:36 +0900

    回答

--- tenka1-2014-quala/a/main.go
+++ tenka1-2014-quala/a/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
+	"sort"
+	"strconv"
+)
 
+func main() {
+	n := 1000
+	ans := make([]string, n)
+	for i := range ans {
+		ans[i] = strconv.Itoa(i + 1)
+	}
+	sort.Strings(ans)
+
+	wr := bufio.NewWriter(os.Stdout)
+	for _, v := range ans {
+		wr.WriteString(v + "\n")
+	}
+	wr.Flush()
 }
