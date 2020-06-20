73ea7c6c8322ce95b1bd22bc6a3ea8df88ea8ecb
matsutake <matsutake.pg@gmail.com>
2020-06-20 18:22:25 +0900

    2回分割

--- code-formula-2014-final/b/main.go
+++ code-formula-2014-final/b/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	if n%2 == 0 {
+		fmt.Println(n / 2)
+	} else {
+		fmt.Println((n-1)/2 + 3)
+	}
 }
--- code-formula-2014-final/c/main.go
+++ code-formula-2014-final/c/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
+	"sort"
+	"strings"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func init() {
+	sc.Split(bufio.ScanLines)
+	sc.Buffer(make([]byte, 100001), 100001*100)
+}
+
+func main() {
+	s := scanString()
+	xm := make(map[string]bool)
+	for _, ss := range strings.Split(s, " ") {
+		for i, sss := range strings.Split(ss, "@") {
+			if i >= 1 {
+				xm[sss] = true
+			}
+		}
+	}
+	delete(xm, "")
+
+	xs := make([]string, 0, len(xm))
+	for k := range xm {
+		xs = append(xs, k)
+	}
+	sort.Strings(xs)
+	wr := bufio.NewWriter(os.Stdout)
+	for _, v := range xs {
+		wr.WriteString(v + "\n")
+	}
+	wr.Flush()
 }
