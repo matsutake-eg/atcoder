007e5405aa89f7ad32de78401d8774cce22d11ed
matsutake <matsutake.pg@gmail.com>
2020-06-22 05:38:53 +0900

    回答

--- code-festival-2015-qualb/b/main.go
+++ code-festival-2015-qualb/b/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n, _ := scanInt(), scanInt()
+	xm := make(map[int]int)
+	for i := 0; i < n; i++ {
+		a := scanInt()
+		xm[a]++
+		if xm[a]*2 > n {
+			fmt.Println(a)
+			return
+		}
+	}
+	fmt.Println("?")
 }
--- code-festival-2015-qualb/c/main.go
+++ code-festival-2015-qualb/c/main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n, m := scanInt(), scanInt()
+	a := make([]int, n)
+	for i := range a {
+		a[i] = scanInt()
+	}
+	sort.Ints(a)
+	b := make([]int, m)
+	for i := range b {
+		b[i] = scanInt()
+	}
+	sort.Ints(b)
+
+	p := 0
+	for i := range b {
+		for j := p; j < len(a); j++ {
+			if b[i] <= a[j] {
+				if i == len(b)-1 {
+					fmt.Println("YES")
+					return
+				}
+				p = j + 1
+				break
+			}
+		}
+	}
+	fmt.Println("NO")
 }
