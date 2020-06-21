143beca42a6d584155bba282f3cc8bdc3001e127
matsutake <matsutake.pg@gmail.com>
2020-06-22 01:41:40 +0900

    ロボットアーム

--- mujin-pc-2016/a/main.go
+++ mujin-pc-2016/a/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var c string
+	fmt.Scan(&c)
+
+	switch c {
+	case "O", "P", "K", "L":
+		fmt.Println("Right")
+	default:
+		fmt.Println("Left")
+	}
 }
--- mujin-pc-2016/b/main.go
+++ mujin-pc-2016/b/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+	"sort"
+)
 
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	xs := make([]int, 3)
+	r1 := 0.0
+	for i := range xs {
+		fmt.Scan(&xs[i])
+		r1 += float64(xs[i])
+	}
+	sort.Ints(xs)
+	r2 := 0.0
+	if xs[0]+xs[1] < xs[2] {
+		r2 = float64(xs[2] - xs[1] - xs[0])
+	}
+	fmt.Println((r1*r1 - r2*r2) * math.Pi)
 }
