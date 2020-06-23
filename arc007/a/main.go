2af4b32e89f9875d2ffb191d66a512bf61b16072
matsutake <matsutake.pg@gmail.com>
2020-06-23 18:45:35 +0900

    回答

--- arc007/a/main.go
+++ arc007/a/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	var x, s string
+	fmt.Scan(&x, &s)
+
+	fmt.Println(strings.ReplaceAll(s, x, ""))
 }
--- arc007/b/main.go
+++ arc007/b/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n, m := scanInt(), scanInt()
+	xs := make([]int, n+1)
+	for i := range xs {
+		xs[i] = i
+	}
+
+	for i := 0; i < m; i++ {
+		d := scanInt()
+		for j := range xs {
+			if xs[j] == d {
+				xs[j] = xs[0]
+				xs[0] = d
+				break
+			}
+		}
+	}
+
+	wr := bufio.NewWriter(os.Stdout)
+	for _, v := range xs[1:] {
+		wr.WriteString(strconv.Itoa(v) + "\n")
+	}
+	wr.Flush()
 }
