ff67a27af785db7f4230c2f6fcd1193e51b79444
matsutake <matsutake.pg@gmail.com>
2020-06-23 19:15:24 +0900

    回答

--- arc008/a/main.go
+++ arc008/a/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	fmt.Println(min(100*(n/10)+15*(n%10), 100*((n+9)/10)))
 }
--- arc008/b/main.go
+++ arc008/b/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import "fmt"
 
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	var (
+		n, m      int
+		name, kit string
+	)
+	fmt.Scan(&n, &m, &name, &kit)
+
+	nm := make(map[rune]int)
+	for _, r := range name {
+		nm[r]++
+	}
+	km := make(map[rune]int)
+	for _, r := range kit {
+		km[r]++
+	}
+
+	ans := 0
+	for k, v := range nm {
+		if _, ok := km[k]; !ok {
+			fmt.Println(-1)
+			return
+		}
+
+		ans = max(ans, (v+km[k]-1)/km[k])
+	}
+	fmt.Println(ans)
 }
