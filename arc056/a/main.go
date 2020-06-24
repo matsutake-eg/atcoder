72ff97614a0dc6c08ca8b0ebd5c67cf57984f74d
matsutake <matsutake.pg@gmail.com>
2020-06-24 18:42:41 +0900

    回答

--- arc056/a/main.go
+++ arc056/a/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	var a, b, k, l int
+	fmt.Scan(&a, &b, &k, &l)
+
+	x, y := k/l, k%l
+	fmt.Println(min(a*y+b*x, b*(x+1)))
 }
