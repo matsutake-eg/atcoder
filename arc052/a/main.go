f7bf1f4f58a390929d11e57a6b3df52a478dc1b8
matsutake <matsutake.pg@gmail.com>
2020-06-25 20:58:40 +0900

    回答

--- arc052/a/main.go
+++ arc052/a/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	var s string
+	fmt.Scan(&s)
+
+	wr := bufio.NewWriter(os.Stdout)
+	for _, r := range s {
+		sr := string(r)
+		for i := 0; i <= 9; i++ {
+			if strings.Contains(sr, strconv.Itoa(i)) {
+				wr.WriteString(sr)
+				break
+			}
+		}
+	}
+	wr.WriteString("\n")
+	wr.Flush()
 }
