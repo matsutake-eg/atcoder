3d1eb772744d4820df6c19e868125ff8c0d225fa
matsutake <matsutake.pg@gmail.com>
2020-06-25 09:25:09 +0900

    回答

--- arc012/a/main.go
+++ arc012/a/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var day string
+	fmt.Scan(&day)
+
+	switch day {
+	case "Saturday", "Sunday":
+		fmt.Println(0)
+	case "Monday":
+		fmt.Println(5)
+	case "Tuesday":
+		fmt.Println(4)
+	case "Wednesday":
+		fmt.Println(3)
+	case "Thursday":
+		fmt.Println(2)
+	case "Friday":
+		fmt.Println(1)
+	}
 }
--- arc012/b/main.go
+++ arc012/b/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var (
+		n         int
+		va, vb, l float64
+	)
+	fmt.Scan(&n, &va, &vb, &l)
+
+	for i := 0; i < n; i++ {
+		t := l / va
+		l = vb * t
+	}
+	fmt.Println(l)
 }
