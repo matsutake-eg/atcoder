cd397787e35687e4bc56103fda245796ef311144
matsutake <matsutake.pg@gmail.com>
2020-06-27 18:22:34 +0900

    回答

--- arc013/b/main.go
+++ arc013/b/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	c := scanInt()
+	xs := make([][]int, c)
+	mn, mm, ml := 0, 0, 0
+	for i := range xs {
+		xs[i] = make([]int, 3)
+		for j := range xs[i] {
+			xs[i][j] = scanInt()
+		}
+		sort.Ints(xs[i])
+		mn = max(mn, xs[i][0])
+		mm = max(mm, xs[i][1])
+		ml = max(ml, xs[i][2])
+	}
+	fmt.Println(mn * mm * ml)
 }
