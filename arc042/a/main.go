bc60765c06671398ccc6ba9b8ff01b7a38cae120
matsutake <matsutake.pg@gmail.com>
2020-06-27 18:40:09 +0900

    回答

--- arc042/a/main.go
+++ arc042/a/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n, m := scanInt(), scanInt()
+	a := make([]int, m)
+	for i := range a {
+		a[i] = scanInt()
+	}
+
+	xs := make([]bool, n+1)
+	wr := bufio.NewWriter(os.Stdout)
+	for i := m - 1; i >= 0; i-- {
+		if xs[a[i]] {
+			continue
+		}
+		wr.WriteString(strconv.Itoa(a[i]) + "\n")
+		xs[a[i]] = true
+	}
+	for i := 1; i <= n; i++ {
+		if !xs[i] {
+			wr.WriteString(strconv.Itoa(i) + "\n")
+		}
+	}
+	wr.Flush()
 }
