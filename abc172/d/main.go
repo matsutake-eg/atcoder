d0d020be6b7a59e21fcbced8fa7ffe7f2c6833d2
matsutake <matsutake.pg@gmail.com>
2020-06-27 23:39:24 +0900

    計算量を落とすように修正

--- abc172/d/main.go
+++ abc172/d/main.go
@@ -6,18 +6,11 @@ func main() {
 	var n int
 	fmt.Scan(&n)
 
-	xs := make([]int, n+1)
-	for i := range xs {
-		xs[i] = 1
-	}
+	ans := (n + 1) * n / 2
 	for i := 2; i <= n; i++ {
 		for j := 1; i*j <= n; j++ {
-			xs[i*j]++
+			ans += i * j
 		}
-	}
-	ans := 0
-	for i := 1; i <= n; i++ {
-		ans += i * xs[i]
 	}
 	fmt.Println(ans)
 }
