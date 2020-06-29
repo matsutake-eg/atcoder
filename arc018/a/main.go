7fc8d5c3d6b27fd69ab1afe0c7e9fa000a2dc889
matsutake <matsutake.pg@gmail.com>
2020-06-29 15:36:37 +0900

    回答

--- arc018/a/main.go
+++ arc018/a/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	var h, b float64
+	fmt.Scan(&h, &b)
+
+	fmt.Println(b * math.Pow((h/1000.0), 2) * 100)
 }
--- arc019/a/main.go
+++ arc019/a/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	var s string
+	fmt.Scan(&s)
+
+	wr := bufio.NewWriter(os.Stdout)
+	for _, r := range s {
+		switch r {
+		case 'O', 'D':
+			wr.WriteString("0")
+		case 'I':
+			wr.WriteString("1")
+		case 'Z':
+			wr.WriteString("2")
+		case 'S':
+			wr.WriteString("5")
+		case 'B':
+			wr.WriteString("8")
+		default:
+			wr.WriteString(string(r))
+		}
+	}
+	wr.WriteString("\n")
+	wr.Flush()
 }
