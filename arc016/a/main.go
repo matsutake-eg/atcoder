bfa1bb7333d83a65a1479fdafac10125675e3adc
matsutake <matsutake.pg@gmail.com>
2020-06-29 18:50:13 +0900

    回答

--- arc016/a/main.go
+++ arc016/a/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n, m int
+	fmt.Scan(&n, &m)
+
+	xs := make([]int, 0, n-1)
+	for i := 1; i <= n; i++ {
+		if i != m {
+			xs = append(xs, i)
+		}
+	}
+	fmt.Println(xs[0])
 }
--- arc016/b/main.go
+++ arc016/b/main.go
@@ -1,5 +1,49 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	iv, _ := strconv.Atoi(scanString())
+	return iv
+}
+
+func scanString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 100000), 100000000)
+}
+
+func main() {
+	n := scanInt()
+	xs := make([]string, n)
+	ans := 0
+	cnt := make([]int, 9)
+	for i := range xs {
+		xs[i] = scanString()
+		for j, r := range xs[i] {
+			switch r {
+			case 'x':
+				ans++
+			case 'o':
+				if i == 0 || xs[i-1][j] != 'o' {
+					cnt[j]++
+				}
+			}
+		}
+	}
+	for _, v := range cnt {
+		ans += v
+	}
+	fmt.Println(ans)
 }
