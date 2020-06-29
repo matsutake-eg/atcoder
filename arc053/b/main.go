0fc0dd85fb4d5b5102b8a33fb9bd19014102742b
matsutake <matsutake.pg@gmail.com>
2020-06-29 18:28:35 +0900

    回答

--- arc053/b/main.go
+++ arc053/b/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 100000), 100000000)
+}
+
+func main() {
+	s := scanString()
+	xm := make(map[rune]int)
+	for _, r := range s {
+		xm[r]++
+	}
+	o, e := 0, 0
+	for _, v := range xm {
+		o += v % 2
+		e += v / 2
+	}
+	if o == 0 {
+		fmt.Println(e * 2)
+	} else {
+		fmt.Println(1 + e/o*2)
+	}
 }
