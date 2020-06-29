b422ff9bb6faad78525b6e54c6c5fc295701213a
matsutake <matsutake.pg@gmail.com>
2020-06-29 15:54:27 +0900

    回答

--- arc053/a/main.go
+++ arc053/a/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var h, w int
+	fmt.Scan(&h, &w)
+
+	ans := 0
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			if i+1 < h {
+				ans++
+			}
+			if j+1 < w {
+				ans++
+			}
+		}
+	}
+	fmt.Println(ans)
 }
