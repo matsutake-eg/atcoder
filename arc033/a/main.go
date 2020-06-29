37876ccc4fb816978a91e1104e975582c68e2c35
matsutake <matsutake.pg@gmail.com>
2020-06-29 19:01:27 +0900

    回答

--- arc033/a/main.go
+++ arc033/a/main.go
@@ -1,5 +1,9 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n int
+	fmt.Scan(&n)
+	fmt.Println((1 + n) * n / 2)
 }
--- arc033/b/main.go
+++ arc033/b/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	na, nb := scanInt(), scanInt()
+	xm := make(map[int]bool)
+	for i := 0; i < na; i++ {
+		a := scanInt()
+		xm[a] = true
+	}
+	cnt := 0
+	for i := 0; i < nb; i++ {
+		b := scanInt()
+		if xm[b] {
+			cnt++
+		} else {
+			xm[b] = true
+		}
+	}
+	fmt.Println(float64(cnt) / float64(len(xm)))
 }
