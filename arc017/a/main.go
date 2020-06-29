e019e0c5443a8bf4a52cc7d94494bfa28591a696
matsutake <matsutake.pg@gmail.com>
2020-06-30 06:22:29 +0900

    回答

--- arc017/a/main.go
+++ arc017/a/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+func isPrime(x int) bool {
+	if x < 2 {
+		return false
+	}
+
+	for i := 2; i*i <= x; i++ {
+		if x%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	if isPrime(n) {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
 }
--- arc017/b/main.go
+++ arc017/b/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n, k := scanInt(), scanInt()
+	b := scanInt()
+	cnt := 1
+	ans := 0
+	for i := 1; i < n; i++ {
+		a := scanInt()
+		if a > b {
+			cnt++
+		} else {
+			cnt = 1
+		}
+		if cnt >= k {
+			ans++
+		}
+		b = a
+	}
+	if k == 1 {
+		fmt.Println(n)
+	} else {
+		fmt.Println(ans)
+	}
 }
