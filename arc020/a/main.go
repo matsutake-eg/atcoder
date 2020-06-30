cd8749d9700955c1c1f94f2831bcea0c10844164
matsutake <matsutake.pg@gmail.com>
2020-07-01 08:15:08 +0900

    制約から全探索が使えるか考える

--- arc020/a/main.go
+++ arc020/a/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+
+	a = abs(a)
+	b = abs(b)
+	if a == b {
+		fmt.Println("Draw")
+	} else if a < b {
+		fmt.Println("Ant")
+	} else {
+		fmt.Println("Bug")
+	}
 }
--- arc020/b/main.go
+++ arc020/b/main.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	n, c := scanInt(), scanInt()
+	xm := make(map[int]int)
+	ym := make(map[int]int)
+	for i := 0; i < n; i++ {
+		a := scanInt()
+		if i%2 == 0 {
+			xm[a]++
+		} else {
+			ym[a]++
+		}
+	}
+
+	sum := math.MaxInt64
+	for i := 1; i <= 10; i++ {
+		for j := 1; j <= 10; j++ {
+			if i == j {
+				continue
+			}
+			sum = min(sum, n-xm[i]-ym[j])
+		}
+	}
+	fmt.Println(sum * c)
 }
