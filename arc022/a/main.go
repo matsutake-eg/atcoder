84e1864170a23bc0fac8ccd3a29aaa903558325d
matsutake <matsutake.pg@gmail.com>
2020-07-01 07:28:00 +0900

    回答

--- arc022/a/main.go
+++ arc022/a/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	var s string
+	fmt.Scan(&s)
+
+	i, c := false, false
+	for _, r := range strings.ToLower(s) {
+		switch r {
+		case 'i':
+			i = true
+		case 'c':
+			if i {
+				c = true
+			}
+		case 't':
+			if c {
+				fmt.Println("YES")
+				return
+			}
+		}
+	}
+	fmt.Println("NO")
 }
