5d164079fbd2bea40a6e3a60d042735db2700c87
matsutake <matsutake.pg@gmail.com>
2020-07-02 09:47:22 +0900

    回答

--- arc023/a/main.go
+++ arc023/a/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func calc(y, m, d int) int {
+	return 365*y + y/4 - y/100 + y/400 + (306 * (m + 1) / 10) + d - 429
+}
+
+func main() {
+	var y, m, d int
+	fmt.Scan(&y, &m, &d)
+
+	if m == 1 || m == 2 {
+		y--
+		m += 12
+	}
+	fmt.Println(calc(2014, 5, 17) - calc(y, m, d))
 }
--- arc023/b/main.go
+++ arc023/b/main.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	r, c, d := scanInt(), scanInt(), scanInt()
+	a := make([][]int, r)
+	for i := range a {
+		a[i] = make([]int, c)
+		for j := range a[i] {
+			a[i][j] = scanInt()
+		}
+	}
+
+	ans := math.MinInt64
+	for i := 0; i < min(d+1, r); i++ {
+		for j := (i + d) % 2; j < min(d+1-i, c); j += 2 {
+			ans = max(ans, a[i][j])
+		}
+	}
+	fmt.Println(ans)
 }
