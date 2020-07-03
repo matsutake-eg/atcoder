969e5ac52ed330360dc98ce06fa37a556e92be0c
matsutake <matsutake.pg@gmail.com>
2020-07-03 20:18:16 +0900

    約数の和

--- arc026/a/main.go
+++ arc026/a/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n, a, b int
+	fmt.Scan(&n, &a, &b)
+
+	if n <= 5 {
+		fmt.Println(b * n)
+	} else {
+		fmt.Println(b*5 + a*(n-5))
+	}
 }
--- arc026/b/main.go
+++ arc026/b/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	sum := 1
+	for i := 2; i*i <= n; i++ {
+		if i*i == n {
+			sum += i
+		} else if n%i == 0 {
+			sum += i
+			sum += n / i
+		}
+	}
+	if n == 1 {
+		fmt.Println("Deficient")
+	} else if sum == n {
+		fmt.Println("Perfect")
+	} else if sum < n {
+		fmt.Println("Deficient")
+	} else {
+		fmt.Println("Abundant")
+	}
 }
