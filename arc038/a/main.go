922ae09b48ff0a6f97955ae660d5f4aa4a376220
matsutake <matsutake.pg@gmail.com>
2020-07-04 22:04:12 +0900

    回答

--- arc038/a/main.go
+++ arc038/a/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n := scanInt()
+	a := make([]int, n)
+	for i := range a {
+		a[i] = scanInt()
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(a)))
+
+	ans := 0
+	for i := range a {
+		if i%2 == 0 {
+			ans += a[i]
+		}
+	}
+	fmt.Println(ans)
 }
--- arc039/a/main.go
+++ arc039/a/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import "fmt"
 
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+
+	a1 := a % 10
+	a2 := a%100 - a1
+	a3 := a - a2 - a1
+
+	b1 := b % 10
+	b2 := b%100 - b1
+	b3 := b - b2 - b1
+
+	ans := a - b
+	ans = max(ans, 900+a2+a1-b)
+	ans = max(ans, a3+90+a1-b)
+	ans = max(ans, a3+a2+9-b)
+	ans = max(ans, a-(100+b2+b1))
+	ans = max(ans, a-(b3+b1))
+	ans = max(ans, a-(b3+b2))
+	fmt.Println(ans)
 }
