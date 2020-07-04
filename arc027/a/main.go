0d57d639a14b332d91172971f8e3eae6ba82a6b2
matsutake <matsutake.pg@gmail.com>
2020-07-04 13:35:32 +0900

    回答

--- arc027/a/main.go
+++ arc027/a/main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var h, m int
+	fmt.Scan(&h, &m)
+
+	fmt.Println((18-h)*60 - m)
 }
--- arc028/a/main.go
+++ arc028/a/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n, a, b int
+	fmt.Scan(&n, &a, &b)
+
+	for {
+		n -= a
+		if n <= 0 {
+			fmt.Println("Ant")
+			return
+		}
+		n -= b
+		if n <= 0 {
+			fmt.Println("Bug")
+			return
+		}
+	}
 }
