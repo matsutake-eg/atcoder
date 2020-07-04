141dcaac7faedc2a754163505d7ee5ef1e696ccb
matsutake <matsutake.pg@gmail.com>
2020-07-04 22:26:48 +0900

    回答

--- arc029/a/main.go
+++ arc029/a/main.go
@@ -1,5 +1,57 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	n := scanInt()
+	t := make([]int, n)
+	for i := range t {
+		t[i] = scanInt()
+	}
+
+	ans := math.MaxInt64
+	for i := 1; i < 1<<uint(n); i++ {
+		sum1, sum2 := 0, 0
+		for j := 0; j < n; j++ {
+			if b := 1 << uint(j); i&b == b {
+				sum1 += t[j]
+			} else {
+				sum2 += t[j]
+			}
+		}
+		ans = min(ans, max(sum1, sum2))
+	}
+	fmt.Println(ans)
 }
--- arc030/a/main.go
+++ arc030/a/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n, k int
+	fmt.Scan(&n, &k)
+
+	if k <= n/2 {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
 }
