ed0d439f9a27f0b7f0f1de1b42a4f5ef6890091a
matsutake <matsutake.pg@gmail.com>
2020-07-06 07:24:25 +0900

    最大値を入れてみて考察

--- arc034/a/main.go
+++ arc034/a/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
 
+func max(x, y float64) float64 {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	n := scanInt()
+	ans := math.SmallestNonzeroFloat64
+	for i := 0; i < n; i++ {
+		a, b, c, d, e := scanInt(), scanInt(), scanInt(), scanInt(), scanInt()
+		ans = max(ans, float64(a+b+c+d)+float64(e*110)/900)
+	}
+	fmt.Println(ans)
 }
--- arc034/b/main.go
+++ arc034/b/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
 
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	ans := make([]int, 0, 154)
+	for i := 0; i <= 153; i++ {
+		x := n - i
+		if x <= 0 {
+			break
+		}
+		sx := strconv.Itoa(x)
+		sum := x
+		for _, r := range sx {
+			ri, _ := strconv.Atoi(string(r))
+			sum += ri
+		}
+		if sum == n {
+			ans = append(ans, x)
+		}
+	}
+	sort.Ints(ans)
+
+	wr := bufio.NewWriter(os.Stdout)
+	wr.WriteString(strconv.Itoa(len(ans)) + "\n")
+	for _, v := range ans {
+		wr.WriteString(strconv.Itoa(v) + "\n")
+	}
+	wr.Flush()
 }
