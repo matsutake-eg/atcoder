be8e7e7354aeaeb7176f822f8355edc8df78ad3c
matsutake <matsutake.pg@gmail.com>
2020-07-05 19:04:25 +0900

    回答

--- arc032/a/main.go
+++ arc032/a/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+func isPrime(x int) bool {
+	if x < 2 {
+		return false
+	}
+
+	for i := 2; i*i <= x; i++ {
+		if x%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	sum := (1 + n) * n / 2
+	if isPrime(sum) {
+		fmt.Println("WANWAN")
+	} else {
+		fmt.Println("BOWWOW")
+	}
 }
--- arc032/b/main.go
+++ arc032/b/main.go
@@ -1,5 +1,69 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	iv, _ := strconv.Atoi(sc.Text())
+	return iv
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+}
+
+var (
+	par  []int
+	rank []int
+)
+
+func initTree(x int) {
+	par = make([]int, x+1)
+	rank = make([]int, x+1)
+	for i := range par {
+		par[i] = i
+	}
+}
+
+func find(x int) int {
+	if par[x] == x {
+		return x
+	}
+	par[x] = find(par[x])
+	return par[x]
+}
+
+func union(x, y int) {
+	px, py := find(x), find(y)
+	if px == py {
+		return
+	}
+	if rank[px] < rank[py] {
+		par[px] = py
+		return
+	} else if rank[px] == rank[py] {
+		rank[px]++
+	}
+	par[py] = px
+}
+
+func main() {
+	n, m := scanInt(), scanInt()
+	initTree(n)
+	for i := 0; i < m; i++ {
+		a, b := scanInt(), scanInt()
+		union(a, b)
+	}
+	xm := make(map[int]bool)
+	for i := 1; i <= n; i++ {
+		xm[find(i)] = true
+	}
+	fmt.Println(len(xm) - 1)
 }
