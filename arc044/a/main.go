8b4d3de2b6c850f301e3221d9c35e700da3e7a44
matsutake <matsutake.pg@gmail.com>
2020-07-06 06:59:42 +0900

    回答

--- arc044/a/main.go
+++ arc044/a/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+func isPrime(x int) bool {
+	if x < 2 {
+		return false
+	}
+
+	for i := 2; i*i <= x; i++ {
+		if x%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	if isPrime(n) {
+		fmt.Println("Prime")
+	} else if n == 1 {
+		fmt.Println("Not Prime")
+	} else {
+		sn := strconv.Itoa(n)
+		switch sn[len(sn)-1] {
+		case '1', '3', '7', '9':
+			sum := 0
+			for _, r := range sn {
+				x, _ := strconv.Atoi(string(r))
+				sum += x
+			}
+			if sum%3 != 0 {
+				fmt.Println("Prime")
+				return
+			}
+		}
+		fmt.Println("Not Prime")
+	}
 }
