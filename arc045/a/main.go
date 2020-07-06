e00eaabde411e4d5f4517725da5e0a49cbbc7421
matsutake <matsutake.pg@gmail.com>
2020-07-07 07:22:42 +0900

    回答

--- arc045/a/main.go
+++ arc045/a/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
+	"strings"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func init() {
+	sc.Split(bufio.ScanLines)
+	sc.Buffer(make([]byte, 100000), 100000000)
+}
+
+func main() {
+	s := scanString()
+	xs := strings.Split(s, " ")
+	xm := make(map[string]string)
+	xm["Left"] = "<"
+	xm["Right"] = ">"
+	xm["AtCoder"] = "A"
+	wr := bufio.NewWriter(os.Stdout)
+	for i, v := range xs {
+		wr.WriteString(xm[v])
+		if i < len(xs)-1 {
+			wr.WriteString(" ")
+		} else {
+			wr.WriteString("\n")
+		}
+	}
+	wr.Flush()
 }
