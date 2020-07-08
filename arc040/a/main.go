19d665acf4d883ca6746c4fc4ca010dfe52e1f6f
matsutake <matsutake.pg@gmail.com>
2020-07-08 19:26:42 +0900

    いかちゃん

--- arc040/a/main.go
+++ arc040/a/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	iv, _ := strconv.Atoi(scanString())
+	return iv
+}
+
+func scanString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func init() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 100000), 100000000)
+}
+
+func main() {
+	n := scanInt()
+	rc, bc := 0, 0
+	for i := 0; i < n; i++ {
+		s := scanString()
+		for _, r := range s {
+			switch r {
+			case 'R':
+				rc++
+			case 'B':
+				bc++
+			}
+		}
+	}
+	if rc == bc {
+		fmt.Println("DRAW")
+	} else if rc > bc {
+		fmt.Println("TAKAHASHI")
+	} else {
+		fmt.Println("AOKI")
+	}
 }
--- arc040/b/main.go
+++ arc040/b/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import "fmt"
 
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	var (
+		n, r int
+		s    string
+	)
+	fmt.Scan(&n, &r, &s)
+
+	ans := 0
+	for i := n - 1; i >= 0; i-- {
+		if s[i] == '.' {
+			ans += max(0, i-r+1)
+			break
+		}
+	}
+	for i := 0; i < n; i++ {
+		if s[i] == '.' {
+			ans++
+			i += r - 1
+		}
+	}
+	fmt.Println(ans)
 }
