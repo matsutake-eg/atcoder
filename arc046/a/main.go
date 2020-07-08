a5bd38537b49fcbf1ba84410dab9678a5ec10c5d
matsutake <matsutake.pg@gmail.com>
2020-07-08 18:51:40 +0900

    ゾロ目数

--- arc046/a/main.go
+++ arc046/a/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	x := (n-1)/9 + 1
+	r := (n-1)%9 + 1
+	wr := bufio.NewWriter(os.Stdout)
+	for i := 0; i < x; i++ {
+		wr.WriteString(strconv.Itoa(r))
+	}
+	wr.WriteString("\n")
+	wr.Flush()
 }
