10a3205f702e47d6444c46694f635fa5bf6da6bc
matsutake <matsutake.pg@gmail.com>
2020-07-09 09:07:43 +0900

    回答

--- arc047/a/main.go
+++ arc047/a/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var (
+		n, l int
+		s    string
+	)
+	fmt.Scan(&n, &l, &s)
+
+	ans := 0
+	cnt := 0
+	for _, r := range s {
+		switch r {
+		case '+':
+			cnt++
+		case '-':
+			if cnt > 0 {
+				cnt--
+			}
+		}
+		if cnt == l {
+			ans++
+			cnt = 0
+		}
+	}
+	fmt.Println(ans)
 }
