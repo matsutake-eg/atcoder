fecbdb7f30697bb4aba2c78f05ec94ef3e073efb
matsutake <matsutake.pg@gmail.com>
2020-07-10 09:22:34 +0900

    arc035をarc036で解いてたので修正

--- arc035/a/main.go
+++ arc035/a/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var s string
+	fmt.Scan(&s)
+
+	for i := 0; i*2 <= len(s); i++ {
+		if r1, r2 := s[i], s[len(s)-1-i]; r1 == '*' || r2 == '*' || r1 == r2 {
+			continue
+		}
+		fmt.Println("NO")
+		return
+	}
+	fmt.Println("YES")
 }
