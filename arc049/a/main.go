ea07a3a0e40bb880298a12a2edb069b50e4aa037
matsutake <matsutake.pg@gmail.com>
2020-07-11 05:11:10 +0900

    回答

--- arc049/a/main.go
+++ arc049/a/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var (
+		s          string
+		a, b, c, d int
+	)
+	fmt.Scan(&s, &a, &b, &c, &d)
+
+	fmt.Println(s[:a] + "\"" + s[a:b] + "\"" + s[b:c] + "\"" + s[c:d] + "\"" + s[d:])
 }
