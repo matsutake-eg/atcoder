ea1afff581a3a1b6e7a61e1992557784637245f8
matsutake <matsutake.pg@gmail.com>
2020-07-10 09:17:38 +0900

    回答

--- arc048/a/main.go
+++ arc048/a/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+
+	if a < 0 && b > 0 {
+		fmt.Println((b - a - 1))
+	} else {
+		fmt.Println(b - a)
+	}
 }
