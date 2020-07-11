88afe6f3d81bd13e48e87b7f75fb48dac56c35b5
matsutake <matsutake.pg@gmail.com>
2020-07-12 00:21:28 +0900

    回答

--- arc050/a/main.go
+++ arc050/a/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	var C, c string
+	fmt.Scan(&C, &c)
+
+	if strings.ToLower(C) == c {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
 }
