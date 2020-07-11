d9b82e0eebab6c11cf943aa33858b8d67fad57ae
matsutake <matsutake.pg@gmail.com>
2020-07-12 00:47:00 +0900

    円の内包

--- arc051/a/main.go
+++ arc051/a/main.go
@@ -1,17 +1,38 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
 
 func main() {
-	var C, c string
-	fmt.Scan(&C, &c)
+	var x1, y1, r int
+	fmt.Scan(&x1, &y1, &r)
+	var x2, y2, x3, y3 int
+	fmt.Scan(&x2, &y2, &x3, &y3)
 
-	if strings.ToLower(C) == c {
-		fmt.Println("Yes")
+	x1l, x1r := x1-r, x1+r
+	y1t, y1b := y1+r, y1-r
+	if x1l <= x2 && x3 <= x1r && y1b <= y2 && y3 <= y1t {
+		for _, v := range [][]int{{x2, y2}, {x2, y3}, {x3, y2}, {x3, y3}} {
+			d2 := (x1-v[0])*(x1-v[0]) + (y1-v[1])*(y1-v[1])
+			if d2 > r*r {
+				fmt.Println("YES")
+				fmt.Println("YES")
+				return
+			}
+		}
+		fmt.Println("YES")
+		fmt.Println("NO")
+	} else if x2 <= x1l && x1r <= x3 && y2 <= y1b && y1t <= y3 {
+		fmt.Println("NO")
+		fmt.Println("YES")
 	} else {
-		fmt.Println("No")
+		fmt.Println("YES")
+		fmt.Println("YES")
 	}
 }
