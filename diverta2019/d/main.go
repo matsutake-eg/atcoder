770004dc741b94d3f995e1cb0273732562a8b2a4
Takeshi Matsuda <matsutake@MBP.local>
2020-07-18 23:57:46 +0900

    式変形

--- diverta2019/d/main.go
+++ diverta2019/d/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	ans := 0
+	for i := 1; i*i < n; i++ {
+		if x := n % i; x == 0 {
+			if y := (n / i) - 1; y > i {
+				ans += y
+			}
+		}
+	}
+	fmt.Println(ans)
 }
