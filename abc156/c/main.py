967fff1ec4d9150c08220d7193de7cb015e8c0f9
matsutake <matsutake.pg@gmail.com>
2020-07-29 07:53:59 +0900

    繰り返し二乗法

--- abc156/c/main.py
+++ abc156/c/main.py
@@ -1,1 +1,10 @@
-0
+n = int(input())
+x = list(map(int, input().split()))
+
+ans = 10 ** 9
+for i in range(1, 101):
+    sm = 0
+    for v in x:
+        sm += (v - i) ** 2
+    ans = min(ans, sm)
+print(ans)
