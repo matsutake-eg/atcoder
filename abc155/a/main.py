fed18bdeb39e0d1909c49dfe0f6fb13e3343a641
matsutake <matsutake.pg@gmail.com>
2020-07-31 09:18:26 +0900

    解き直し

--- abc155/a/main.py
+++ abc155/a/main.py
@@ -1,1 +1,5 @@
-0
+a, b, c = map(int, input().split())
+if (a == b and b == c and c == a) or (a != b and b != c and c != a):
+    print("No")
+else:
+    print("Yes")
--- abc155/b/main.py
+++ abc155/b/main.py
@@ -1,1 +1,9 @@
-0
+n = int(input())
+a = list(map(int, input().split()))
+
+for v in a:
+    if v % 2 == 0:
+        if not (v % 3 == 0 or v % 5 == 0):
+            print("DENIED")
+            exit()
+print("APPROVED")
