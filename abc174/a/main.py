518c18499a6d63330a335bc55b6bf4af0c169a3b
matsutake <matsutake.pg@gmail.com>
2020-08-05 01:17:09 +0900

    解き直し

--- abc174/a/main.py
+++ abc174/a/main.py
@@ -1,1 +1,5 @@
-0
+x = int(input())
+if x >= 30:
+    print("Yes")
+else:
+    print("No")
--- abc174/c/main.py
+++ abc174/c/main.py
@@ -1,1 +1,9 @@
-0
+k = int(input())
+x = 0
+for i in range(1, k + 1):
+    x = x * 10 + 7
+    if x % k == 0:
+        print(i)
+        exit()
+    x %= k
+print(-1)
