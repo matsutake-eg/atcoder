33eb2f2c25952aa9f001c583369c222bf130bc24
matsutake <matsutake.pg@gmail.com>
2020-08-05 01:55:32 +0900

    全探索と累積和

--- abc174/d/main.py
+++ abc174/d/main.py
@@ -1,1 +1,16 @@
-0
+n = int(input())
+c = input()
+
+r = 0
+for v in c:
+    if v == "R":
+        r += 1
+ans = min(r, n - r)
+w = 0
+for v in c:
+    if v == "W":
+        w += 1
+    else:
+        r -= 1
+    ans = min(ans, max(w, r))
+print(ans)
