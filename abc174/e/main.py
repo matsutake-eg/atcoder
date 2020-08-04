eea1d9a2bd8ebe0a06c058043ac9146441901706
matsutake <matsutake.pg@gmail.com>
2020-08-05 03:44:19 +0900

    答えで二分探索

--- abc174/e/main.py
+++ abc174/e/main.py
@@ -1,1 +1,11 @@
-0
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+l, r = 1, 10 ** 9
+while l < r:
+    m = (l + r) // 2
+    if sum(map(lambda v: (v - 1) // m, a)) > k:
+        l = m + 1
+    else:
+        r = m
+print(l)
