bd7cc700b9a84afd58bed924736ee9be9d0a7caa
matsutake <matsutake.pg@gmail.com>
2020-08-11 19:02:11 +0900

    連結成分の個数

--- abc173/f/main.py
+++ abc173/f/main.py
@@ -1,1 +1,12 @@
-0
+from functools import reduce
+
+n = int(input())
+
+E = reduce(lambda ac, v: ac + v * (n - v + 1), range(1, n + 1), 0)
+V = 0
+for _ in range(n - 1):
+    a, b = map(int, input().split())
+    if a > b:
+        a, b = b, a
+    V += a * (n - b + 1)
+print(E - V)
