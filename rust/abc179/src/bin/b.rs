7f0e4c59ea5199d916a6cac5f971f6ea5220107d
Takeshi Matsuda <matsutake.pg@gmail.com>
2021-01-05 09:50:27 +0900

    過去問

---
+++ rust/abc179/src/bin/b.rs
@@ -0,0 +1,37 @@
+#![allow(unused_imports)]
+use itertools::Itertools;
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::*;
+use proconio::fastout;
+use proconio::input;
+use proconio::marker::*;
+use std::cmp::*;
+use std::collections::*;
+use std::f64::consts::*;
+
+#[allow(unused)]
+const MOD: usize = 1_000_000_007;
+
+// CAUTION: fastout has bug that it is not work in 'match arm', so, in that case, you should remove it or change logic.
+#[proconio::fastout]
+fn main() {
+    proconio::input! {
+        n:usize,
+        d: [(usize,usize);n],
+    }
+
+    let mut cnt = 0;
+    for (d1, d2) in d {
+        if d1 == d2 {
+            cnt += 1;
+            if cnt == 3 {
+                println!("Yes");
+                return;
+            }
+        } else {
+            cnt = 0;
+        }
+    }
+    println!("No");
+}
