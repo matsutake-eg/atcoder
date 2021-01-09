80882de6059e042005d595d920abadea30c92a84
Takeshi Matsuda <matsutake.pg@gmail.com>
2021-01-09 22:21:15 +0900

    二分探索と累積和

--- rust/abc181/src/bin/e.rs
+++ rust/abc181/src/bin/e.rs
@@ -19,12 +19,47 @@ fn main() {
     proconio::input! {
         n:usize,
         m:usize,
-        h:[usize;n],
-        w:[usize;m],
+        h:[i64;n],
+        w:[i64;m],
     }
 
     let mut h = h;
     h.sort();
-    println!("{:?}", h);
-    println!("{:?}", w);
+
+    let mut d1 = vec![0; n];
+    let mut i = 0;
+    while i < d1.len() - 1 {
+        d1[i] = (h[i] - h[i + 1]).abs();
+        i += 2;
+    }
+    let d1 = d1.iter().cumsum().collect::<Vec<i64>>();
+
+    let mut d2 = vec![0; n];
+    let mut i = 1;
+    while i < d2.len() - 1 {
+        d2[i] = (h[i] - h[i + 1]).abs();
+        i += 2;
+    }
+    let d2 = d2.iter().cumsum().collect::<Vec<i64>>();
+
+    let mut ans = std::i64::MAX;
+    for v in w {
+        let mut d = 0;
+        let idx = h.binary_search(&v).unwrap_or_else(|x| x);
+        if idx % 2 == 0 {
+            d += (v - h[idx]).abs();
+            d += d2[n - 1] - d2[idx];
+            if idx > 0 {
+                d += d1[idx - 1];
+            }
+        } else {
+            d += (v - h[idx - 1]).abs();
+            d += d2[n - 1] - d2[idx - 1];
+            if idx > 1 {
+                d += d1[idx - 2];
+            }
+        }
+        ans = min(ans, d);
+    }
+    println!("{}", ans);
 }
