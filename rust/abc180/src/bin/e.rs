2e4584fd24ad7aa4c7ad4674d8863dc97701ac47
Takeshi Matsuda <matsutake.pg@gmail.com>
2021-01-09 14:08:55 +0900

    巡回セールスマン問題とビットDP

--- rust/abc180/src/bin/e.rs
+++ rust/abc180/src/bin/e.rs
@@ -17,14 +17,40 @@ const MOD: usize = 1_000_000_007;
 #[proconio::fastout]
 fn main() {
     proconio::input! {
-        // n:usize,
-        // a:i64,
-        // f:f64,
-        // s:String,
-        // t:Chars,
-        // a:[usize;n],
-        // ab: [(usize,usize);n],
-        // ab: [(Usize1, Usize1); m],
-        // a:[[usize;m];n],
+        n:usize,
+        xyz: [(i64,i64,i64);n],
     }
+
+    let mut dist = vec![vec![0; n]; n];
+    for i in 0..n {
+        for j in 0..n {
+            let mut now = (xyz[i].0 - xyz[j].0).abs();
+            now += (xyz[i].1 - xyz[j].1).abs();
+            now += max(0, xyz[j].2 - xyz[i].2);
+            dist[i][j] = now as usize;
+        }
+    }
+
+    let n2 = 1 << n;
+    let mut dp = vec![vec![100_000_000_000; n]; n2];
+    for i in 0..n {
+        if i == 0 {
+            continue;
+        }
+        dp[1 << i][i] = dist[0][i];
+    }
+    for i in 0..n2 {
+        for j in 0..n {
+            if i >> j & 1 != 1 {
+                continue;
+            }
+            for k in 0..n {
+                if i >> k & 1 == 1 {
+                    continue;
+                }
+                dp[i | 1 << k][k] = min(dp[i | 1 << k][k], dp[i][j] + dist[j][k]);
+            }
+        }
+    }
+    println!("{}", dp[n2 - 1][0]);
 }
