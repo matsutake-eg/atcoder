ca9928fca7da9b16fe488cc8cd70d4c2436ecd54
Takeshi Matsuda <matsutake.pg@gmail.com>
2021-01-21 21:47:52 +0900

    回答

---
+++ rust/abc186/src/bin/c.rs
@@ -0,0 +1,30 @@
+#![allow(unused_imports)]
+use itertools::Itertools;
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::*;
+use proconio::fastout;
+use proconio::input;
+use proconio::marker::*;
+use std::cmp::*;
+use std::collections::*;
+use std::f64::consts::*;
+
+#[allow(unused)]
+const MOD: usize = 1_000_000_007;
+
+// CAUTION: fastout has bug that it is not work in 'match arm', so, in that case, you should remove it or change logic.
+#[proconio::fastout]
+fn main() {
+    proconio::input! {
+        n:usize,
+    }
+
+    let mut ans = n;
+    for i in 1..=n {
+        if i.to_string().contains("7") || format!("{:o}", i).to_string().contains("7") {
+            ans -= 1;
+        }
+    }
+    println!("{}", ans);
+}
