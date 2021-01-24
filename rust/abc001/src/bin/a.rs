65c8bb99155a8bc4f70b8275fce76c58278c7b97
Takeshi Matsuda <matsutake.pg@gmail.com>
2021-01-24 20:45:37 +0900

    風力観測😑

---
+++ rust/abc001/src/bin/a.rs
@@ -0,0 +1,25 @@
+#![allow(unused_imports)]
+use itertools::Itertools;
+use itertools_num::ItertoolsNum as _;
+use num_integer::*;
+use petgraph::*;
+use proconio::fastout;
+use proconio::input;
+use proconio::marker::*;
+use std::cmp::*;
+use std::collections::*;
+use std::f64::consts::*;
+
+#[allow(unused)]
+const MOD: usize = 1_000_000_007;
+
+// CAUTION: fastout has bug that it is not work in 'match arm', so, in that case, you should remove it or change logic.
+#[proconio::fastout]
+fn main() {
+    proconio::input! {
+        h1:i64,
+        h2:i64,
+    }
+
+    println!("{}", h1 - h2);
+}
