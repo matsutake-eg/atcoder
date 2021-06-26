ee70e8f1dc7b420536b38a192dc5c1a9c6c21aff
Takeshi Matsuda <matsutake.pg@gmail.com>
2021-06-27 04:19:47 +0900

    競プロ典型 90 問問題追加

--- rust/typical90/src/bin/004.rs
+++ rust/typical90/src/bin/004.rs
@@ -3,43 +3,24 @@ use itertools::Itertools;
 use itertools_num::ItertoolsNum as _;
 use num_integer::*;
 use petgraph::*;
-use proconio::marker::*;
+use proconio::{fastout, input, marker::*};
 use std::cmp::*;
 use std::collections::*;
 use std::f64::consts::*;
+use superslice::Ext as _;
 
-// CAUTION: fastout has bug that it is not work in 'match arm', so, in that case, you should remove it or change logic.
-#[proconio::fastout]
+// CAUTION: don't print in 'match arm' because of fastout's bug
+#[fastout]
 fn main() {
-    proconio::input! {
-        h:usize,
-        w:usize,
-        a:[[usize;w];h],
-    }
-
-    let mut row = vec![0; h];
-    let mut col = vec![0; w];
-    for i in 0..h {
-        for j in 0..w {
-            row[i] += a[i][j];
-            col[j] += a[i][j];
-        }
-    }
-
-    let mut ans = vec![vec![0; w]; h];
-    for i in 0..h {
-        for j in 0..w {
-            ans[i][j] = row[i] + col[j] - a[i][j];
-        }
-    }
-
-    for i in 0..h {
-        for j in 0..w {
-            print!("{}", ans[i][j]);
-            if j < w - 1 {
-                print!(" ");
-            }
-        }
-        println!("");
+    input! {
+        // n:usize,
+        // a:i64,
+        // f:f64,
+        // s:String,
+        // t:Chars,
+        // a:[usize;n],
+        // ab: [(usize,usize);n],
+        // ab: [(Usize1, Usize1);m],
+        // a:[[usize;m];n],
     }
 }
